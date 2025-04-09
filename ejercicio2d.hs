42f1b0506825aeca99b2ccc60f3ce2aba9e886b5
Tu Nombre <you@example.com>
2025-04-09 13:05:21 -0300

    new file:   ejercicio2d.hs

---
+++ ejercicio2d.hs
@@ -0,0 +1,5 @@
+--dados dos números racionales, decide si alguno es igual a 0 (resolverlo con y sin pattern matching)
+algunoEsCero ::  Float -> Float -> Bool
+algunoEsCero x y | x == 0 = True
+                 | y == 0 = True 
+                 | otherwise = False
