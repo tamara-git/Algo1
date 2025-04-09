e5c3656d851ff7c5a8d850c7ffa373fc8e3d741d
tamara <tamaradiana.ar@gmail.com>
2025-04-08 22:52:22 -0300

    archivo con la composición de funciones f y g

---
+++ ejercicio1c.hs
@@ -0,0 +1,16 @@
+{- A partir de las funciones definidas en a y b, implementar las funciones parciales h=fog y k=gof -}
+f :: Integer -> Integer
+f(1)=8
+f(4)=131
+f(16)=16
+
+g :: Integer -> Integer
+g(8)= 16
+g(16)=4
+g(131)=1
+
+h :: Integer -> Integer
+h = (fog)
+
+k :: Integer -> Integer
+k = (gof)
