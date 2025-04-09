e0b2759a72ecafc971ff2b108b567a483aa42872
tamara <tamaradiana.ar@gmail.com>
2025-04-09 19:48:32 -0300

    modified:   ambosSonCero.hs

--- ambosSonCero.hs
+++ ambosSonCero.hs
@@ -1,5 +1,17 @@
 ambosSonCero ::  Float -> Float -> Bool
-ambosSonCero x y | (x == 0 = True) && (y == 0 = True)
+ambosSonCero x y |  (x==0 = True) && (y==0 = True)
+                 | otherwise = False
+
+--Con Pattern Matching
+algunoEsCero2 :: Float -> Float -> Bool
+algunoEsCero2 x 0 = True
+algunoEsCero2 0 y = True
+algunoEsCero2 x y = False
+--Con Pattern Matching
+algunoEsCero2 :: Float -> Float -> Bool
+algunoEsCero2 x 0 = True
+algunoEsCero2 0 y = True
+algunoEsCero2 x y = False
                  | otherwise = False
 
 --Con Pattern Matching
