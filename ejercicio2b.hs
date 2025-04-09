b56687df6f0298b88ac95067f21f29ef58439011
Tu Nombre <you@example.com>
2025-04-09 11:50:30 -0300

    modified:   ejercicio2b.hs

--- ejercicio2b.hs
+++ ejercicio2b.hs
@@ -1,4 +1,8 @@
-
+absoluto :: Integer -> Integer
+absoluto x  | x < 0 = -x
+            | x >= 0 = x
 
 maximoAbsoluto :: Integer -> Integer -> Integer
-maximoAbsoluto x y |  
+maximoAbsoluto x y | absoluto x >= absoluto y = absoluto x
+                   | otherwise absoluto y
+                
