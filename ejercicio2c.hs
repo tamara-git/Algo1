576a9bb2ac94f925bb73896a2e6ace8c79349593
Tu Nombre <you@example.com>
2025-04-09 12:26:49 -0300

    modified:   ejercicio2c.hs

--- ejercicio2c.hs
+++ ejercicio2c.hs
@@ -1,8 +1,8 @@
 maximo2 :: Integer -> Integer
-maximo2 x y | x >= y = x
+maximo2 x y | (x >= y) = x
             | otherwise = y
       
 
 maximo3 :: Integer -> Integer -> Integer -> Integer
-maximo3 x y z | maximo2 >= z = maximo2
+maximo3 x y z | (maximo2 >= z) = maximo2
               | otherwise = z
