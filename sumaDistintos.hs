0a3d54b4b95d517749936f534a24afc1822834a3
tamara <tamaradiana.ar@gmail.com>
2025-04-10 13:58:41 -0300

    Puse mal un ->

--- sumaDistintos.hs
+++ sumaDistintos.hs
@@ -2,5 +2,5 @@ sumaDos :: Integer -> Integer -> Integer
 sumaDos x y | x /= y = x+y
             | otherwise x
 
-sumaDistintos :: Integer -> Integer -> Integer -Integer
+sumaDistintos :: Integer -> Integer -> Integer -> Integer
 sumaDistintos x y z | sumaDos (sumaDos x y) z
