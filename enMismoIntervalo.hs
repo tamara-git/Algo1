2f42c31b782d9e504d5cc472025cc7e11af5b6fa
tamara <tamaradiana.ar@gmail.com>
2025-04-10 11:16:43 -0300

    .

--- enMismoIntervalo.hs
+++ enMismoIntervalo.hs
@@ -3,6 +3,6 @@ Intervalo x | (x <= 3) = 'A'
             | (x > 3 && x <= 7 ) = 'B'
             | ( x > 7) = 'C'
 
-enMismoIntervalo :: Float -> Float -> Char -> Bool
+enMismoIntervalo :: Float -> Float -> Bool
 enMismoIntervalo x y  | (Intervalo x == Intervalo y) = True
                       | otherwise = False
