87e51bd63f2802c45f257bc4fe2914d43f8a1003
tamara <tamaradiana.ar@gmail.com>
2025-04-10 13:18:16 -0300

    modifiqué cambios

--- enMismoIntervalo.hs
+++ enMismoIntervalo.hs
@@ -1,8 +1,8 @@
-Intervalo :: Float -> Char
-Intervalo x | (x <= 3) = 'A'
+intervalo :: Float -> Char
+intervalo x | (x <= 3) = 'A'
             | (x > 3) && (x <= 7 ) = 'B'
             | ( x > 7) = 'C'
 
 enMismoIntervalo :: Float -> Float -> Bool
-enMismoIntervalo x y  = (Intervalo x == Intervalo y)
+enMismoIntervalo x y  = (intervalo x == intervalo y)
                       | otherwise = False                     
