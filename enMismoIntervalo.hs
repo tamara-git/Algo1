6f2e085831b2188a66242cbdc47dfb0e44dea0c4
tamara <tamaradiana.ar@gmail.com>
2025-04-10 10:55:34 -0300

    modificaciones

--- enMismoIntervalo.hs
+++ enMismoIntervalo.hs
@@ -1,8 +1,8 @@
-Intervalo :: Float -> Bool
-Intervalo x  | x <= 3
-             | 3 < x <= 7
-             | x > 7
+Intervalo :: Float -> Char
+Intervalo x | (x <= 3) = ClaseEquivalencia1
+             | (3 < x <= 7) = ClaseEquivalencia2
+             | ( x > 7) = ClaseEquivalencia3
 
-enMismoIntervalo :: Float -> Float -> Bool
+enMismoIntervalo :: Float -> Char -> Bool
 enMismoIntervalo x y | Intervalo x == Intervalo y = True
                      | otherwise = False
