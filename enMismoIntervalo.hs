b354ffabc0f478f79900db32aa6911dd81e791ce
tamara <tamaradiana.ar@gmail.com>
2025-04-10 10:44:09 -0300

    Funcion mismo intervalo

---
+++ enMismoIntervalo.hs
@@ -0,0 +1,8 @@
+Intervalo :: Float -> Bool
+Intervalo x  | x <= 3
+             | 3 < x <= 7
+             | x > 7
+
+enMismoIntervalo :: Float -> Float -> Bool
+enMismoIntervalo x y | Intervalo x == Intervalo y = True
+                     | otherwise False
