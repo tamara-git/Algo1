990d82936d1bb9f1c77f66b5e6480b312efecca0
tamara <tamaradiana.ar@gmail.com>
2025-04-10 13:59:50 -0300

    me faltó el = en otherwise

--- sumaDistintos.hs
+++ sumaDistintos.hs
@@ -1,6 +1,6 @@
 sumaDos :: Integer -> Integer -> Integer
 sumaDos x y | x /= y = x+y
-            | otherwise x
+            | otherwise = x
 
 sumaDistintos :: Integer -> Integer -> Integer -> Integer
 sumaDistintos x y z | sumaDos (sumaDos x y) z
