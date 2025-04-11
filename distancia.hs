87120b966017445771e38d70c20a2a2912854d13
tamara <tamaradiana.ar@gmail.com>
2025-04-10 23:22:15 -0300

    distancia.hs modificado

--- distancia.hs
+++ distancia.hs
@@ -1,1 +1,7 @@
 --Calcula la distancia euclídea entre dos puntos de RxR
+type Punto2D = (Float,Float)
+distancia_al_cuadrado :: Punto2D -> Float
+distancia_al_cuadrado (a,b) (c,d) = (a-c)**2 + (b-d)**2
+
+distancia :: Punto2D -> Float
+distancia (a,b) (c,d) = sqrt (distancia_al_cuadrado)
