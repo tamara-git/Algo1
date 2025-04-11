c5a671bf7ce4a37cdbf588edc056d90f8b3edad3
tamara <tamaradiana.ar@gmail.com>
2025-04-11 14:24:31 -0300

    correciones de sumarSoloMultiplos.hs

--- sumarSoloMultiplos.hs
+++ sumarSoloMultiplos.hs
@@ -1,11 +1,12 @@
 {-Dada una terna de números enteros y un natural, calcula la suma de los elementos de la terna que son 
 múltiplos del número natural.-}
 esMultiploDe :: Integer -> Integer -> Bool
-esMultiploDe x y = mod x y == 0
+esMultiploDe x y | mod x y == 0 = x
+                 | otherwise 0
+                 
 
 sumarSoloMultiplos :: (Integer, Integer, Integer) -> Integer -> Integer
-sumarSoloMultiplos (a,b,c) d  | (esMultiploDe a d || esMultiploDe b d || esMultiploDe c d) = (a+b+c)
-                              | otherwise = 0
+sumarSoloMultiplos (a,b,c) d  | esMultiploDe a d + esMultiploDe b d + esMultiploDe c d 
 
                               
                             
