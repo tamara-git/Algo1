d4739b396f070cbab83329d78ae589b5c4cfc5a5
tamara <tamaradiana.ar@gmail.com>
2025-04-11 17:13:30 -0300

    cambios distanciaManhattan.hs

--- distanciaManhattan.hs
+++ distanciaManhattan.hs
@@ -6,7 +6,7 @@
 type Punto3D = (Float, Float, Float)
 
 absoluto :: Punto3D -> Punto3D
-absoluto (a,b,c) (d,e,f) | (a,b,c)-(d,e,f) = (d,e,f) - (a,b,c)
+absoluto (a,b,c) (d,e,f) | (a+b+c) - (d+e+f) = (d+e+f) - (a+b+c)
                  
 distanciaManhattan :: Punto3D -> Punto3D -> Float
 distanciaManhattan (a,b,c) (d,e,f) = absoluto (a,b,c) (d,e,f)
