083aa164732f2e0d7e7bcfb68336b9937f41c7a2
tamara <tamaradiana.ar@gmail.com>
2025-04-11 16:00:22 -0300

    problema bisiesto.hs

---
+++ bisiesto.hs
@@ -0,0 +1,9 @@
+{- problema bisiesto (año:Z) : Bool {
+    requiere: {True}
+    asegura: {(res=false) ↔ (año no es múltiplo de 4, o bien, año es múltiplo de 100 pero no de 400)
+} -}
+type Anio = Integer
+type EsBisiesto = Bool
+
+bisiesto :: Integer -> Bool 
+bisiesto x = not ((mod x 4 /= 0) && (mod x 100) && (mod x 400 /= 0))
