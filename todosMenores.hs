17df4d0e6efc2bf177cba342c084ba57cbb420ed
tamara <tamaradiana.ar@gmail.com>
2025-04-11 15:35:20 -0300

    todosMenores.hs contiene todos los ejercicios del punto 5

---
+++ todosMenores.hs
@@ -0,0 +1,24 @@
+type Terna = (Integer, Integer, Integer)
+{-problema todosMenores (t: ZxZxZ) : Bool {
+    requiere: {True}
+    asegura: {(res=true) ↔ (f(t0) > g(t0) ∧ (f(t1) > g(t1)) ∧ (f(t2) > g(t2)))}
+}-}
+todosMenores :: Terna -> Bool
+todosMenores (a,b,c) | (f(a) > g(a)) && (f(b) > g(b)) && (f(c) > g(c))
+
+{-problema f (n:Z) : Z {
+    requiere: {True}
+    asegura: {(n <= 7 →  res = n*2) ∧ (n > 7 →  res= 2n-1 )
+} -}
+f :: Integer -> Integer
+f n | n <= 7 = n*2
+    | n > 7 = 2n-1
+
+{- problema g (n:Z) : Z {
+    requiere: {True}
+    asegura: {Si n es un número par entonces res = n/2, en caso contrario, res = 3n+1}
+} -}
+g :: Integer -> Integer
+g n |  mod n 2 == 0 = n/2
+    | otherwise = 3n+1
+    
