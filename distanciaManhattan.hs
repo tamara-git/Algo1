4d4e13833b34612a72c2c52cd171232055736466
tamara <tamaradiana.ar@gmail.com>
2025-04-11 16:39:46 -0300

    resolví distanciaManhattan.hs

---
+++ distanciaManhattan.hs
@@ -0,0 +1,12 @@
+{-problema distanciaManhattan (p: RxRxR, q: RxRxR) : R {
+   requiere: {True}
+   asegura: {res = Sumatoria de i=0 a 2 |pi - qi|}
+} -}
+
+type Punto3D = (Float, Float, Float)
+
+absoluto :: Punto3D -> Punto3D
+absoluto (a,b,c) (d,e,f) | (a,b,c)-(d,e,f) = (d,e,f) - (a,b,c)
+                 
+distanciaManhattan :: Punto3D -> Punto3D -> Float
+distanciaManhattan (a,b,c) (d,e,f) = absoluto (a,b,c) (d,e,f)
