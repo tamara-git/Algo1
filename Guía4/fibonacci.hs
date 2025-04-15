e036ae7584a9260878a5d20241c9ff670acca52c
tamara <tamaradiana.ar@gmail.com>
2025-04-15 18:57:03 -0300

    new file:   "Gu\303\255a4/fibonacci.hs" 	new file:   "Gu\303\255a4/parteEntera.hs"

---
+++ Guía4/fibonacci.hs
@@ -0,0 +1,17 @@
+{-Implementar la funci´on fibonacci:: Integer ->Integer que devuelve el i-´esimo n´umero de Fibonacci.
+Recordar que la secuencia de Fibonacci se define como:
+   f ib(n) =
+0 si n = 0
+1 si n = 1
+f ib(n − 1) + f ib(n − 2) en otro caso 
+
+problema fibonacci (n: Z) : Z {
+requiere: { n ≥ 0 }
+asegura: { resultado = f ib(n) }
+}
+-}
+ fibonacci :: Integer -> Integer
+ fibonacci n  | n==o = 0
+                  | n==1 = 1
+                  | otherwise = fibonacci (n-1) + fibonacci (n-2)
+                  
