157a3ef61514e9e7c96150be28d708f835f5f074
tamara <tamaradiana.ar@gmail.com>
2025-04-17 11:22:29 -0300

    hice el ej. todosDigitosIguales

---
+++ Guía4/todosDigitosIguales.hs
@@ -0,0 +1,12 @@
+{- problema todosDigitosIguales (n:Z) : B  {
+    requiere: {n > 0}
+    asegura: {resultado=true ⟺ todos los dígitos de n son iguales}
+}
+-}
+
+decena :: Integer -> Integer
+decena x = 10*x
+
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales x  |  x < 10 = False 
+                       | otherwise = todosDigitosIguales (x - div (x 2)) + div ( x 2)
