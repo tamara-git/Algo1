09fbe7791179932868febc84bfc3a89019063e71
tamara <tamaradiana.ar@gmail.com>
2025-04-17 09:32:18 -0300

    realicé medioFact

---
+++ Guía4/medioFact.hs
@@ -0,0 +1,10 @@
+{- problema medioFact (n:Z) :Z  {
+    requiere: {n >= 0}
+    asegura: {resultado =  productoria i=0  [(n-1)/2]  (n-2i)}
+}
+-}
+
+medioFact :: Integer -> Integer
+medioFact 0 = 1
+medioFact 1 = 1
+medioFact n = medioFact (n-2) *  n
---
+++ Guía4/parteEntera.hs
@@ -0,0 +1,10 @@
+{-problema parteEntera (x: R) : Z {
+requiere: { x ≥ 0 }
+asegura: { resultado ≤ x < resultado + 1 }
+} 
+-}
+parteEntera :: Float -> Integer
+parteEntera x  | x < 1 = 0
+               | otherwise = 1 + parteEntera (x-1)
+               
+                
