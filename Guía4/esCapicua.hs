986e1144f1976e1f4370e56965165c9f2e62ee2d
tamara <tamaradiana.ar@gmail.com>
2025-04-17 17:37:04 -0300

    a ver

--- Guía4/esCapicua.hs
+++ Guía4/esCapicua.hs
@@ -4,4 +4,4 @@
 esCapicua :: Integer -> Bool
 esCapicua x  | x <= 10 = False 
              | mod x 10 == 0 = False
-             | (esCapicua (x-1) + (esCapicua (x) - esCapicua (x-1))) = True 
+             | (esCapicua (x-1) + (esCapicua (x) - esCapicua (x-1))) == esCapicua (x) = True 
