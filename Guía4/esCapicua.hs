933c552876f36525f95acb38323c7d0da73e0757
tamara <tamaradiana.ar@gmail.com>
2025-04-17 16:36:15 -0300

    modifiqué la función y AGREGUÉ otro caso base

--- Guía4/esCapicua.hs
+++ Guía4/esCapicua.hs
@@ -1,3 +1,4 @@
 esCapicua :: Integer -> Bool
- esCapicua x  | x < 10 = False 
-             | esCapicua (esCapicua (x-1)) + 1 = True
+esCapicua x  | x < 10 = False 
+             | x == 10 = 01
+             | esCapicua (x-1) = True 
