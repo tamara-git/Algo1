08e9d8967797c16dfd566a9a3c2e581bc8463efd
tamara <tamaradiana.ar@gmail.com>
2025-04-17 16:58:11 -0300

    ahora creo que sí está bien

--- Guía4/esCapicua.hs
+++ Guía4/esCapicua.hs
@@ -3,4 +3,4 @@
 esCapicua :: Integer -> Bool
 esCapicua x  | x <= 10 = False 
              | mod x 10 == 0 = False
-             | esCapicua (x-1) + 10 = True 
+             | esCapicua (x-1) + (x-(x-1)) = True 
