91b6443a6c5867f0a1432c48073a820b41605e05
tamara <tamaradiana.ar@gmail.com>
2025-04-17 15:57:27 -0300

    realicé una segunda modificacion

--- Guía4/esCapicua.hs
+++ Guía4/esCapicua.hs
@@ -1,3 +1,3 @@
 esCapicua :: Integer -> Bool
-esCapicua x  | x < 10 = False 
-             | esCapicua (x-1) + 1 = True
+ esCapicua x  | x < 10 = False 
+             | esCapicua (esCapicua (x-1)) + 1 = True
