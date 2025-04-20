29c8948e2770e48ba6eb5dafec8622479f4366a5
tamara <tamaradiana.ar@gmail.com>
2025-04-20 18:43:31 -0300

    mayorDigitoPar

---
+++ Guía4/mayorDigitoPar.hs
@@ -0,0 +1,10 @@
+{-problema mayorDigitoPar (n: N) : N {
+requiere: { True }
+asegura: { resultado es el mayor de los dígitos pares de n. 
+Si n no tiene ningún dígito par, entonces resultado es -1.}
+}-}
+
+mayorDigitoPar :: Integer -> Integer
+mayorDigitoPar n | mod n 2 == 1  = -1
+                 | (n <= 10) && (mod n 2 == 0) = n
+                 | mayorDigitoPar (div n 2)*2 
