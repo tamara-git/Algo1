8871340818d5943cba039f83afe8822e03708468
Tu Nombre <you@example.com>
2025-04-21 11:04:56 -0300

    .

--- Guía4/mayorDigitoPar.hs
+++ Guía4/mayorDigitoPar.hs
@@ -9,4 +9,4 @@ Si n no tiene ningún dígito par, entonces resultado es -1.}
 mayorDigitoPar :: Integer -> Integer
 mayorDigitoPar n | (n < 10 ) && (mod n 2 == 1) = -1
                  | (n < 10 ) && (mod n 2 == 0) = n
-                 | otherwise = mayorDigitoPar (div n 2)*2 
+                 | otherwise = (mayorDigitoPar (div n 2))*2 
