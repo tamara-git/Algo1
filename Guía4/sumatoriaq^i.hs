70c2b531d27eeab067f607a284e5cf5428873a75
tamara-git <tamaradiana.ar>
2025-04-22 16:24:44 -0300

    .

--- Guía4/sumatoriaq^i.hs
+++ Guía4/sumatoriaq^i.hs
@@ -1,3 +1,4 @@
 sumatoriaqn :: Float -> Integer -> Float
-sumatoriaqn q n | n == 1 = q 
-                | otherwise = sumatoriaqn q (n-1) + q^n
+sumatoriaqn q n | n == 0 = 0
+                | n == 1 = q 
+                | otherwise = (sumatoria2_q2n q (2*(n-1))) + q^(2*n)
