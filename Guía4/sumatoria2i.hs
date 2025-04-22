353f65f4fe2c5507b94eb71fd2465a4abf43569a
tamara-git <tamaradiana.ar>
2025-04-22 14:01:44 -0300

    change2

--- Guía4/sumatoria2i.hs
+++ Guía4/sumatoria2i.hs
@@ -1,3 +1,3 @@
 sumatoria2i :: Integer -> Integer -> Integer
-sumatoria2i 2 i | i == 0 = 2^1 
-                | otherwise = sumatoria2i 2 (i-1) + 2^i
+sumatoria2i 2 n | n == 0 = 1 
+                | otherwise = sumatoria2i 2 (n-1) + 2^n
