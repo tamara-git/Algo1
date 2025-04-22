ef20fc0c9f63edb20b19f6be2805597a6394c700
tamara-git <tamaradiana.ar>
2025-04-22 16:21:34 -0300

    nuevos cambios

--- Guía4/sumatoria2_(q^2i).hs
+++ Guía4/sumatoria2_(q^2i).hs
@@ -1,3 +1,4 @@
 sumatoria2_q2n :: Float -> Integer -> Float
 sumatoria2_q2n q n | n == 0 = 1
+                   | n == 1 == (q^1 + q^2)
                    | otherwise = sumatoria2_q2n q (2*(n-1)) + q^(2*n)
