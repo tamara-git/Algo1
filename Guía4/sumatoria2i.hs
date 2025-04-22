a29fb90597f5bb8765180369d17d64740b91d748
tamara-git <tamaradiana.ar>
2025-04-22 13:48:26 -0300

    no era n, era 2

--- Guía4/sumatoria2i.hs
+++ Guía4/sumatoria2i.hs
@@ -1,3 +1,3 @@
 sumatoria2i :: Integer -> Integer -> Integer
-sumatoria2i n i | 2^0 == 1 = 1
-                | otherwise = sumatoria2i n (i-1) + 2^n
+sumatoria2i 2 i | 2^0 == 1 = 1
+                | otherwise = sumatoria2i 2 (i-1) + 2^n
