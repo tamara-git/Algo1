11a7ba0beac24470872dec452dd732cc75c4e653
tamara <tamaradiana.ar@gmail.com>
2025-04-22 20:09:29 -0300

    usé la función anterior para usarla en la siguiente

--- Guía4/sumatoriaq^2i.hs
+++ Guía4/sumatoriaq^2i.hs
@@ -1,3 +1,9 @@
+sumatoriaqn :: Float -> Integer -> Float
+sumatoriaqn q n | n == 1 = q 
+                | otherwise = sumatoriaqn q (n-1) + q^n
+
+
+
 sumatoriaq2n :: Float -> Integer -> Float
 sumatoriaq2n q n | n == 0 = 0
-                 | otherwise = (sumatoriaq2n q ((2*n)- 1)) + q^(2*n)
+                 | otherwise = sumatoriaqn q 2*n
