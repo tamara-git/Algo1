c2d8ca176a88e52b4607da3c2d834368ef54537f
tamara <tamaradiana.ar@gmail.com>
2025-04-23 13:45:13 -0300

    e_aprox

--- Guía4/eaprox.hs
+++ Guía4/eaprox.hs
@@ -1,2 +1,8 @@
+{-Especificar e implementar una función eAprox :: Integer ->Float que aproxime el valor del número e
+a partir de la siguiente sumatoria:
+eˆ(n) = sumatoria i=0 n  de 1/i!
+-}
 e_aprox :: Integer -> Float
-e_aprox 
+e_aprox x  | x == 0 = 1
+           | x == 1 = 1
+           | otherwise = div 1 n! + e_aprox (x-1)
