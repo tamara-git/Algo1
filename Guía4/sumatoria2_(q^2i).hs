66fef9addfe983db34f4d527fea99a80fb7f9e05
tamara <tamaradiana.ar@gmail.com>
2025-04-23 09:21:50 -0300

    corregí el último ejercicio del punto 10

--- Guía4/sumatoria2_(q^2i).hs
+++ Guía4/sumatoria2_(q^2i).hs
@@ -1,4 +1,8 @@
+sumatoriaqn :: Float -> Integer -> Float
+sumatoriaqn q n | n == 1 = q 
+                | otherwise = sumatoriaqn q (n-1) + q^n
+
 sumatoria2_q2n :: Float -> Integer -> Float
 sumatoria2_q2n q n | n == 0 = 1
-                   | n == 1 == (q^1 + q^2)
-                   | otherwise = (sumatoria2_q2n q (2*(n-1))) + q^(2*n)
+                   | n == 1 == (q^2 + q^1)
+                   | otherwise = q^(2*n) - q^(n-1)+ sumatoria2_q2n q n-1 - sumatoriaqn q n-1
--- Guía4/sumatoriaq^2i.hs
+++ Guía4/sumatoriaq^2i.hs
@@ -1,5 +1,7 @@
 sumatoriaq2n :: Float -> Integer -> Float
 sumatoriaq2n q n | n == 0 = 0
                  | n == 1 = (q^2 + q^1)
-                 | otherwise = q^(2*n) + q^((2*n)-1) + sumatoriaq2n q (n-1)
+                  n == 2 = q^4 + q^3 +      q^2    
+                  n == 3 = q^6 + q^5  +      q^4 + q^3
+                 | otherwise = 
                  
