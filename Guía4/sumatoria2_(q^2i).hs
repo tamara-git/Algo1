dbeda76b2d6a1bef881018525c43ccb84cb810be
tamara <tamaradiana.ar@gmail.com>
2025-04-23 10:46:54 -0300

    .

--- Guía4/sumatoria2_(q^2i).hs
+++ Guía4/sumatoria2_(q^2i).hs
@@ -4,7 +4,12 @@ sumatoriaqn0 q n | n == 0 = 1
                  | n == 1 = 1 + q 
                  | otherwise = sumatoriaqn q (n-1) + q^n
 
+sumatoriaq2n :: Float -> Integer -> Float
+sumatoriaq2n q n | n == 0 = 0
+                 | n == 1 = (q^2 + q^1)
+                 | otherwise = q^(2*n) + q^(2*n-1) + sumatoriaq2n q (n-1)     
+
 sumatoria2_q2n :: Float -> Integer -> Float
 sumatoria2_q2n q n | n == 0 = 1
                    | n == 1 == (q^2 + q^1)
-                   | otherwise = q^(2*n) + q^(2*n-1) + (sumatoria2_q2n q (n-1)) - (sumatoriaqn q (n-1) )              
+                   | otherwise = sumatoria2_q2n q n - (sumatoriaqn q (n-1))              
