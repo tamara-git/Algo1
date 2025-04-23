928164a23365038b078296d389f3cceb2e674c81
tamara <tamaradiana.ar@gmail.com>
2025-04-23 17:24:53 -0300

    usé el fromInteger

--- Guía4/eaprox.hs
+++ Guía4/eaprox.hs
@@ -3,12 +3,12 @@ a partir de la siguiente sumatoria:
 eˆ(n) = sumatoria i=0 n  de 1/i!
 -}
 -- Intento hacer la recursión en 
-factorial :: Integer -> Integer
+factorial :: Integer -> Float
 factorial x | x == 0 = 1
-            | otherwise = x * factorial (x-1)
+            | otherwise = fromInteger x * factorial (x-1)
 
 e_aprox :: Integer -> Float
-e_aprox x  | x == 0 = 1.0
-           | x == 1 = 2.0
+e_aprox x  | x == 0 = 1
+           | x == 1 = 2
            | otherwise = (factorial x)**(-1) + e_aprox (x-1)
             
