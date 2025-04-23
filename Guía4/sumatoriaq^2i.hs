558f03d1e963e681658b3920641f2f148f7c4046
tamara <tamaradiana.ar@gmail.com>
2025-04-22 23:24:48 -0300

    la recursión se hace nuevamente en el caso anterior

--- Guía4/sumatoriaq^2i.hs
+++ Guía4/sumatoriaq^2i.hs
@@ -1,4 +1,5 @@
 sumatoriaq2n :: Float -> Integer -> Float
-sumatoriaq2n q (2*n) | n == 0 = 0
-                     | n == 1 = (q^2 + q^1)
-                     | otherwise = q^(2*n) + sumatoriaq2n q ((2*n)-1)
+sumatoriaq2n q n | n == 0 = 0
+                 | n == 1 = (q^2 + q^1)
+                 | otherwise = q^(2*n) + q^((2*n)-1) + sumatoriaq2n q (n-1)
+                 
