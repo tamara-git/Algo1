ce5222ffe07c6450ad222718e232e3cb375afb4b
tamara <tamaradiana.ar@gmail.com>
2025-04-23 16:08:16 -0300

    **

--- Guía4/eaprox.hs
+++ Guía4/eaprox.hs
@@ -3,12 +3,12 @@ a partir de la siguiente sumatoria:
 eˆ(n) = sumatoria i=0 n  de 1/i!
 -}
 -- Intento hacer la recursión en 
-factorial :: Float -> Float
+factorial :: Integer -> Integer
 factorial x | x == 0 = 1
             | otherwise = x * factorial (x-1)
 
 e_aprox :: Integer -> Float
 e_aprox x  | x == 0 = 1.0
            | x == 1 = 2.0
-           | otherwise = (factorial x)^(-1) + e_aprox (x-1)
+           | otherwise = (factorial x)**(-1) + e_aprox (x-1)
             
