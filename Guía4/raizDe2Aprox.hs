d54b0eee0b5c1176f3b05241d790a74de5d51924
tamara <tamaradiana.ar@gmail.com>
2025-04-25 07:41:16 -0300

    hic raizDe2Aprox

---
+++ Guía4/raizDe2Aprox.hs
@@ -0,0 +1,24 @@
+{-Lo cual resulta en la siguiente definición recursiva: a1 = 2, an = 2 + 1/an−1.
+ Utilizando esta sucesión, especificar e implementar una función raizDe2Aprox :: Integer -> Float 
+ que dado n ∈ N devuelva la aproximación de √2 definida por √2 ≈ an −1.
+Por ejemplo:
+raizDe2Aprox 1 ⇝ 1
+raizDe2Aprox 2 ⇝ 1,5
+raizDe2Aprox 3 ⇝ 1,4 -}
+{-problema raizDe2Aprox (n:Z) : R {
+    requiere: {n ∈ N}
+    asegura: {resultado = √2 ≈ an−1}
+ }
+-}
+
+--primero hago la recursión en la sucesion
+sucesion :: Float -> Float
+sucesion an | n == 1 = 2
+            | n == 2 = 2.5
+            | otherwise = 2 + 1/a(n-1)
+            
+--uso la sucesión y le resto 1            
+raizDe2Aprox :: Integer -> Float
+raizDe2Aprox 1 = 1
+raizDe2Aprox 2 = 1.5
+raizDe2Aprox n = sucesion an - 1
