db03f23e63a0d6be6806689fc1fce22dd3049499
tamara <tamaradiana.ar@gmail.com>
2025-04-25 23:31:02 -0300

    ejercicio1 Guía5

---
+++ Guía5/longitud.hs
@@ -0,0 +1,25 @@
+--Definir las siguientes funciones sobre listas:
+-- 1. longitud :: [t] -> Integer, que dada una lista devuelve su cantidad de elementos.
+longitud :: [t] -> Integer
+longitud [] = 0
+longitud (x:xs) = 1 + longitud (xs)
+
+{-  2. ultimo :: [t] -> t según la siguiente especificación:
+       problema ultimo (s: seq⟨T⟩) : T {
+            requiere: { |s| > 0 }
+            asegura: { resultado = s[|s| − 1] }
+}
+-}
+ultimo :: [t] -> t 
+ultimo [x] = x
+ultimo (_:xs) = ultimo (xs)
+
+
+{-  3. principio :: [t] -> [t] según la siguiente especificación:
+    problema principio (s: seq⟨T⟩) : seq⟨T⟩ {
+        requiere: { |s| > 0 }
+        asegura: { resultado = subseq(s, 0, |s| − 1) }
+}-}
+principio :: [t] -> [t]
+principio [x] = x
+principio (x:xs) = principio
