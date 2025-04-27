15e45c60ce365df3a4d2f5e4657a9ca19d5f6b26
tamara <tamaradiana.ar@gmail.com>
2025-04-27 15:21:11 -0300

    new changes

--- Guía5/pertenece.hs
+++ Guía5/pertenece.hs
@@ -6,5 +6,5 @@ asegura: { resultado = true ↔ e ∈ s }
 
 pertenece :: (Eq t) => t -> [t] -> Bool 
 pertenece e [] = False
-pertenece e (x:xs) | [x] /= [e] = False
-                   | otherwise = pertenece e [x] || pertenece e (xs) == True 
+pertenece e (x:xs) | [_] /= [e] = False
+                   | otherwise = pertenece e (xs) == True 
