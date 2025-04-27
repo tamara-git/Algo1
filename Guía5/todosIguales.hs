cc384d2768f23f91d5696484f201c246c4cfbda8
tamara <tamaradiana.ar@gmail.com>
2025-04-27 18:44:52 -0300

    .

--- Guía5/todosIguales.hs
+++ Guía5/todosIguales.hs
@@ -4,7 +4,6 @@ que dada una lista devuelve verdadero sí y solamente sí todos sus elementos so
 
 todosIguales :: (Eq t) => [t] -> Bool
 todosIguales [] = True
-todosIguales [x] = True
 todosIguales (x:xs) | (x:xs) == [x] = True
-                    | otherwise = x == todosIguales (xs) 
+                    | otherwise = x == todosIguales (xs) = True
 
