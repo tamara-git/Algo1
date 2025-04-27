6d7bbb7a8e690fedae79250094316074e7ae8c0f
tamara <tamaradiana.ar@gmail.com>
2025-04-27 18:32:15 -0300

    Hecho con Pattern Matching

--- Guía5/todosIguales.hs
+++ Guía5/todosIguales.hs
@@ -4,6 +4,6 @@ que dada una lista devuelve verdadero sí y solamente sí todos sus elementos so
 
 todosIguales :: (Eq t) => [t] -> Bool
 todosIguales [] = True
-todosIguales (x:xs) | [x] == x = True
-                    |otherwise = [x] == todosIguales (xs) 
+todosIguales [x] = True
+todosIguales (x:xs) = todosIguales (xs) ++ [x] 
 
