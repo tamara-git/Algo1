3d9cee4ad6c29e6d223ab8f7a7ac5d4c69ec0f83
tamara <tamaradiana.ar@gmail.com>
2025-04-28 07:50:21 -0300

    Corregido

--- Guía5/quitar.hs
+++ Guía5/quitar.hs
@@ -5,5 +5,5 @@ quitar :: (Eq t) => t -> [t] -> [t]
 quitar x [] = []
 quitar x (xs) | (xs) == [x] = []
               | head xs == x = tail xs
-              | otherwise = quitar x (tail xs)
+              | otherwise = (head xs : quitar x (tail xs))
 
