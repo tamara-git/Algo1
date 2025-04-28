6354cee81ddeb893771ecf3c96311b2f39e124bc
tamara <tamaradiana.ar@gmail.com>
2025-04-27 22:38:58 -0300

    todosDistintos.hs

---
+++ Guía5/todosDistintos.hs
@@ -0,0 +1,11 @@
+{-todosDistintos :: (Eq t) => [t] -> Bool según la siguiente especificación:
+problema todosDistintos (s: seq⟨T⟩) : B {
+requiere: { True }
+asegura: { resultado = false ↔ existen dos posiciones distintas de s con igual valor }
+}-}
+
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs) | (x:xs) == [_] = True
+                      | head xs /= x = todosDistintos (xs)
+                      | otherwise = False
