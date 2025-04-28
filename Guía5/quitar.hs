1a439cc15770aa037a7f70c369ae02ae513746aa
tamara <tamaradiana.ar@gmail.com>
2025-04-28 08:08:09 -0300

    quitarTodos.hs

--- Guía5/quitar.hs
+++ Guía5/quitar.hs
@@ -7,3 +7,16 @@ quitar x (xs) | (xs) == [x] = []
               | head xs == x = tail xs
               | otherwise = (head xs : quitar x (tail xs))
 
+{- quitarTodos :: (Eq t ) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina todas las apariciones
+de x en la lista xs (de haberlas). Es decir:
+problema quitarTodos (e: T, s: seq⟨T⟩) : seq⟨T⟩ {
+requiere: { True }
+asegura: { resultado es igual a s pero sin el elemento e. }
+}-}
+
+quitarTodos :: (Eq t ) => t -> [t] -> [t]
+quitarTodos x [] = []
+quitarTodos x (xs) | (xs) == [x] = []
+                   | head xs == x = tail xs
+                   | tail xs == (x:tail xs) = quitar x (xs) 
+                   | otherwise = quitarTodos (x (quitar x (xs)))
