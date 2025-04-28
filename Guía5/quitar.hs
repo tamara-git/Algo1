b96e7612890cb50aa28228f7788e6aee917709a3
Tu Nombre <you@example.com>
2025-04-28 10:53:14 -0300

    puse []

--- Guía5/quitar.hs
+++ Guía5/quitar.hs
@@ -9,5 +9,5 @@ quitar x (xs) | xs == [x] = []
 {-problema quitarTodos -}
 
 quitarTodos :: (Eq t) => t -> [t] -> [t]
-quitarTodos x xs | head (xs) == x = quitarTodos x (tail xs)
-                 | otherwise = head (xs) ++ quitarTodos x (tail xs)
+quitarTodos x xs | head [xs] == x = quitarTodos x (tail xs)
+                 | otherwise = head [xs] ++ quitarTodos x (tail xs)
