621daadaaf15cc3fb76f788184abb5c3cffd01d0
tamara <tamaradiana.ar@gmail.com>
2025-04-27 22:45:46 -0300

    hayRepetidos.hs hecho

---
+++ Guía5/hayRepetidos.hs
@@ -0,0 +1,11 @@
+{-hayRepetidos :: (Eq t) => [t] -> Bool seg´un la siguiente especificaci´on:
+problema hayRepetidos (s: seq⟨T⟩) : B {
+requiere: { True }
+asegura: { resultado = true ↔ existen dos posiciones distintas de s con igual valor }
+}-}
+
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False 
+hayRepetidos (x:xs) | (x:xs) == [x] = False
+                    | head xs /= x = hayRepetidos (xs)
+                    | otherwise = True 
