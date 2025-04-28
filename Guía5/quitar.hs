edb9db2f1dc20ca0dba2d51d0505adaedf92f857
tamara <tamaradiana.ar@gmail.com>
2025-04-27 23:00:13 -0300

    .

--- Guía5/quitar.hs
+++ Guía5/quitar.hs
@@ -4,6 +4,6 @@ aparición de x en la lista xs (de haberla).-}
 quitar :: (Eq t) => t -> [t] -> [t]
 quitar x [] = []
 quitar x (xs) | xs == [x] = []
-              | head xs /= x = xs
+              | head (xs) /= x = (xs)
               | otherwise = quitar x (head xs:xs) 
 
