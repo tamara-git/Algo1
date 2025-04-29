26b89d1e60e475beb0bab9fd22881324f3368458
tamara <tamaradiana.ar@gmail.com>
2025-04-29 15:49:23 -0300

    me faltó definir el pertenece de hayRepetidos

--- Guía5/eliminarRepetidos.hs
+++ Guía5/eliminarRepetidos.hs
@@ -1,6 +1,11 @@
 {- eliminarRepetidos :: (Eq t) => [t] -> [t] que deja en la lista una única aparición de cada elemento, 
 eliminando las repeticiones adicionales.-}
 
+pertenece :: (Eq t) => t -> [t] -> Bool 
+pertenece e [] = False
+pertenece e (x:xs)  | e == x = True 
+                    | otherwise = pertenece e (xs)
+                    
 hayRepetidos :: (Eq t) => [t] -> Bool 
 hayRepetidos (x:xs) | (x:xs) == [x] = True
                     | pertenece x xs == True = True
