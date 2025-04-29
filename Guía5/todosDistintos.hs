275eacf40c6734cb80c1de800d1602124e9510c0
tamara <tamaradiana.ar@gmail.com>
2025-04-29 09:31:25 -0300

    .

--- Guía5/todosDistintos.hs
+++ Guía5/todosDistintos.hs
@@ -4,14 +4,8 @@ requiere: { True }
 asegura: { resultado = false ↔ existen dos posiciones distintas de s con igual valor }
 }-}
 
-todosIguales :: (Eq t) => [t] -> Bool
-todosIguales [] = True
-todosIguales (x:xs) | (x:xs) == [x] = True
-                    | head xs == x = todosIguales (xs)
-                    | otherwise = False 
-
 todosDistintos :: (Eq t) => [t] -> Bool
 todosDistintos [] = True
 todosDistintos (x:xs) | (x:xs) == [x] = True
-                      | todosIguales (x:xs) == True = False
-                      | otherwise = x:todosDistintos (xs)
+                      | head xs == x = todosDistintos (xs)
+                      | otherwise = False 
