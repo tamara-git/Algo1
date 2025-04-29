7368e1299762d4cc4842b879562a5edc21b89095
tamara <tamaradiana.ar@gmail.com>
2025-04-29 15:42:39 -0300

    .

--- Guía5/eliminarRepetidos.hs
+++ Guía5/eliminarRepetidos.hs
@@ -1,12 +1,7 @@
 {- eliminarRepetidos :: (Eq t) => [t] -> [t] que deja en la lista una única aparición de cada elemento, 
 eliminando las repeticiones adicionales.-}
-hayRepetidos :: (Eq t) => [t] -> Bool
-hayRepetidos [] = False 
-hayRepetidos (x:xs) | (x:xs) == [x] = False
-                    | head xs == x = hayRepetidos (xs)
-                    | otherwise = False 
                                         
 eliminarRepetidos :: (Eq t) => [t] -> [t]
 eliminarRepetidos [x] = [x]
-eliminarRepetidos (x:xs) | x /= head xs = eliminarRepetidos (xs)
+eliminarRepetidos (x:xs) | x == head xs = eliminarRepetidos (xs)
                          | otherwise = x:eliminarRepetidos(xs)
