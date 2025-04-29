7a21c85f613423681498d5e51b65d12c864e44be
tamara <tamaradiana.ar@gmail.com>
2025-04-28 21:35:51 -0300

    cambios actualizados

---
+++ quitar.hs
@@ -0,0 +1,22 @@
+{-quitar :: (Eq t) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina la primera 
+aparición de x en la lista xs (de haberla).-}
+
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar x [] = []
+quitar x (xs) | xs == [x] = []
+              | head (xs) /= x = (xs)
+              | otherwise = quitar x (tail xs) 
+
+{-quitarTodos :: (Eq t ) => t -> [t] -> [t], que dados un entero x y una lista xs, elimina todas las apariciones
+de x en la lista xs (de haberlas). Es decir:
+problema quitarTodos (e: T, s: seq⟨T⟩) : seq⟨T⟩ {
+requiere: { T rue }
+asegura: { resultado es igual a s pero sin el elemento e. }
+}
+-}
+
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos x [] = []
+quitarTodos x (y:xs) | (y:xs) == [x] = []
+                     | y == x = quitarTodos x (xs) 
+                     | otherwise = [y] ++ quitarTodos x (xs)
