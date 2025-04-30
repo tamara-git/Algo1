27a315d8d2148c70c816b4e92bd4d54adf485178
tamara <tamaradiana.ar@gmail.com>
2025-04-30 18:27:56 -0300

    pal /= x

--- ejercicios Parcial/generarStock.hs
+++ ejercicios Parcial/generarStock.hs
@@ -24,4 +24,4 @@ cantidadRepeticiones (pal:xs) | pal == head xs = 1 + cantidadRepeticiones (xs)
                               
 generarStock :: [String] -> [(String, Int)]
 generarStock (x:[]) = [[x],0]
-generarStock (filtrarRepetidos (x:xs)) = [(palabra,cantidadRepeticiones (pal:xs))]
+generarStock (filtrarRepetidos (pal:xs)) = [(palabra,cantidadRepeticiones (pal:xs))]
