766f7e34678e22ffeb8ae2ede4b35335d8463268
tamara <tamaradiana.ar@gmail.com>
2025-04-29 22:03:18 -0300

    sumarN

---
+++ Guía5/sumarN.hs
@@ -0,0 +1,12 @@
+{- sumarN :: Integer -> [Integer] -> [Integer] según la siguiente especificación:
+problema sumarN (n: Z, s: seq⟨Z⟩) : seq⟨Z⟩ {
+requiere: { True }
+asegura: {|resultado| = |s| ∧ cada posición de resultado contiene el valor que hay en esa posición en s sumado
+n }
+}
+-}
+
+sumarN :: Integer -> [Integer] -> [Integer]
+sumarN n [] = [n]
+sumarN [x] = [x + n]
+sumarN n (x:xs) = [x + n] ++ sumarN (xs)
