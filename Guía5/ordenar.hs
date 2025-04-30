9780034077b966e90998b55901c75611d8c15fe7
tamara <tamaradiana.ar@gmail.com>
2025-04-29 22:54:17 -0300

    ordenar.hs

--- Guía5/ordenar.hs
+++ Guía5/ordenar.hs
@@ -1,9 +1,14 @@
+{- ordenar :: [Integer] -> [Integer] que ordena los elementos de la lista en forma creciente. Sugerencia: Pensar
+c´omo pueden usar la funci´on m´ınimo para que ayude a generar la lista ordenada necesaria.
+-}
+
 minimo :: [Integer] -> Integer
 minimo [x] = x
 minimo (x:xs) | x <= minimo xs = x
               | otherwise = minimo (xs)  
 
 ordenar :: [Integer] -> [Integer]
+ordenar [] = []
 ordenar [x] = [x]
-ordenar (x:xs) |  x <= minimo xs = [x] ++ [minimo (xs)]
-               | otherwise = [minimo (xs)] ++ [x]
+ordenar (x:xs) | x <= minimo (xs) = (x:minimo(xs))
+               | otherwise = [minimo(xs)]
