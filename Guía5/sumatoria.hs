d291a02d8621340bd0746a9540f27f55465883f8
tamara <tamaradiana.ar@gmail.com>
2025-04-29 21:21:45 -0300

    sumatoria.hs

---
+++ Guía5/sumatoria.hs
@@ -0,0 +1,14 @@
+{-sumatoria :: [Integer] -> Integer según la siguiente especificación:
+problema sumatoria (s: seq⟨Z⟩) : Z {
+requiere: { True }
+asegura: { resultado = sumatoria desde i=0 hasta |s|−1  s[i] }
+}
+-}
+
+principio :: [t] -> [t]
+principio [x] = []
+principio (x:xs) = x:principio  xs 
+
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = sumatoria (principio (x:xs)) 
