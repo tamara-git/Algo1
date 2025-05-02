1a1806d786d1212fc02a16a8ba6274bcb92f88fc
tamara <tamaradiana.ar@gmail.com>
2025-05-02 00:08:17 -0300

    si está bien le lamo el dedo del pie a Pablo

--- ejercicios Parcial/generarStock.hs
+++ ejercicios Parcial/generarStock.hs
@@ -12,11 +12,11 @@ quitar palabra (_:[]) = []
 quitar palabra (pal:xs) | palabra == pal = quitar palabra xs
                         | otherwise = pal: quitar palabra xs
 
-filtrarRepetidos :: [String] -> [String]
+filtrarRepetidos :: (Eq t) => [t] -> [t]
 filtrarRepetidos (x:[]) = [x] 
 filtrarRepetidos (pal:xs) | pertenece pal xs == True = pal: quitar pal xs
-                          | otherwise = pal: filtrarRepetidos xs
-                        
+                          | otherwise = pal: filtrarRepetidos xs   
+
 cantidadApariciones :: String -> [String] -> Int
 cantidadApariciones y [] = 0
 cantidadApariciones y (x:xs) | y == x = 1 + cantidadApariciones y (xs)
@@ -25,4 +25,4 @@ cantidadApariciones y (x:xs) | y == x = 1 + cantidadApariciones y (xs)
 --generarStock me devuelve la recursión de listas de palabras 
 generarStock :: [String] -> [(String, Int)]
 generarStock [x]= [(x,1)]
-generarStock (y:xs) = [(y,cantidadApariciones y (y:xs))] ++ generarStock xs
+generarStock (y:xs) = filtrar repetidos ([(y,cantidadApariciones y (y:xs))] ++ generarStock xs)
