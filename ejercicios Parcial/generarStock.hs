a381b41b36a42af9999c9045f411c11b234bd865
tamara <tamaradiana.ar@gmail.com>
2025-05-03 10:50:07 -0300

    .

--- ejercicios Parcial/generarStock.hs
+++ ejercicios Parcial/generarStock.hs
@@ -44,6 +44,7 @@ elMasGrande palabra (x:xs) | cantidadApariciones palabra (x:xs) > cantidadAparic
                            | otherwise = [(palabra,cantidadApariciones palabra (xs))] 
 
 filtrarListaTupla :: [String] -> [(String,Int)]
+filtrarListaTupla [] = []
 filtrarListaTupla (x:xs) | head xs == x = elMasGrande x (x:xs) ++ filtrarListaTupla (tail xs)
                          | otherwise = elMasGrande x (x:xs) ++ filtrarListaTupla (xs) 
 
