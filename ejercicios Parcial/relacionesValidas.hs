2c5d37c19d842d35a666eb1b687345bf39860d12
tamara <tamaradiana.ar@gmail.com>
2025-05-03 12:14:16 -0300

    .

--- ejercicios Parcial/relacionesValidas.hs
+++ ejercicios Parcial/relacionesValidas.hs
@@ -7,8 +7,8 @@ componentesDistintas [(palabra1,palabra2)] | palabra1 == palabra2 = False
 
 tuplasNoRepetidas :: Relaciones -> Relaciones -> Bool
 tuplasNoRepetidas [] [] = True
-tuplasNoRepetidas [(palabra1, palabra2)] [(palabra3, palabra4)] | fst [(palabra1,palabra2)] == fst [(palabra3,palabra4)] && snd [(palabra1,palabra2)] == snd [(palabra3,palabra4)] = False
-                                                                | otherwise = True
+tuplasNoRepetidas [(palabra1, palabra2)] [(palabra3, palabra4)] | (palabra1 == palabra3) && (palabra2 == palabra4) = False
+                                                                 | otherwise = True
 {-relacionesValidas :: Relaciones -> Bool
 relacionesValidas [(palabra1,palabra2)] | componentesDistintas == True  && tuplasNoRepetidas == True = True
                                     | otherwise = False
