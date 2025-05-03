6756adcdb498b81cb4c79cf870fd1153b0f0c84c
tamara <tamaradiana.ar@gmail.com>
2025-05-03 10:12:47 -0300

    .

--- ejercicios Parcial/generarStock.hs
+++ ejercicios Parcial/generarStock.hs
@@ -34,9 +34,12 @@ crearTupla (palabra,(x:xs)) = (palabra, cantidadApariciones palabra (x:xs))
 
 generarStock :: [String] -> [(String, Int)]
 generarStock (x:[]) = [(x,1)] 
-generarStock (x:xs) = [(x,cantidadApariciones x (x:xs))] ++ generarStock (xs)
+generarStock (x:xs) | [(x,cantidadApariciones x (x:xs))] ++ generarStock (xs)
 
 --agarrame el de mayor cantidad
 elMasGrande :: String -> [String] -> [(String, Int)] 
-elMasGrande  palabra (x:xs) | cantidadApariciones palabra (x:xs) > cantidadApariciones palabra (xs) = [(palabra,cantidadApariciones palabra (x:xs))]  
+elMasGrande palabra (x:xs) | cantidadApariciones palabra (x:xs) > cantidadApariciones palabra (xs) = [(palabra,cantidadApariciones palabra (x:xs))]  
                    | otherwise = [(palabra,cantidadApariciones palabra (xs))] 
+
+recursiónMasGrande :: [String] -> [(String,Int)]
+recursiónMasGrande (x:xs) = elMasGrande x (x:xs) ++ recursiónMasGrande (xs)
