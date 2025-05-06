099f9c1dbfc8acbae3b0ed99b60c7a9b0fe87ee2
tamara <tamaradiana.ar@gmail.com>
2025-05-06 17:44:53 -0300

    .

--- ejercicios Parcial/relacionesValidas.hs
+++ ejercicios Parcial/relacionesValidas.hs
@@ -4,8 +4,8 @@ componentesDistintas :: [(String,String)] -> Bool
 componentesDistintas [] = True
 componentesDistintas (x:[]) | fst x == snd x = False
                             | otherwise = True
-componentesDistintas (x:xs) | fst x == snd x = componentesDistintas (xs)
-                            | otherwise = True
+componentesDistintas (x:xs) | fst x /= snd x = componentesDistintas (xs)
+                            | otherwise = False
 {-tuplasNoRepetidas :: [(String,String)] -> [(String,String)] -> Bool
 tuplasNoRepetidas [] [] = True
 tuplasNoRepetidas [(a,b)] [(c,d)] | tuplasNoRepetidas
