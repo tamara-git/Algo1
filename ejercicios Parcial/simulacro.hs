507ba2a3f78d0966f27a52364336bd4e797bbd44
tamara <tamaradiana.ar@gmail.com>
2025-05-06 15:38:26 -0300

    .

--- ejercicios Parcial/simulacro.hs
+++ ejercicios Parcial/simulacro.hs
@@ -60,7 +60,8 @@ laQueMasHayQueCodificar frase mapeo = maximoACodificar frase mapeo
 --quiero correr la lista caracter por caracter
 
 codificarCaracteres :: [Char] -> [(Char,Char)] -> [Char]
-codificarCaracteres (x:xs) [] = (x:xs)
+codificarCaracteres (x:[]) (y:ys) | hayQueCodificar x (y:ys) == True = fst y
+                                  | otherwise = x
 codificarCaracteres (x:xs) (y:ys) | hayQueCodificar x (y:ys) == True =  fst y:codificarCaracteres (xs) (y:ys)
                                   | otherwise = x:codificarCaracteres (xs) (y:ys)
 
