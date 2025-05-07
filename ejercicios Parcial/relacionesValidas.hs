1d0be0dd8b0019a551cfee5766e18b90df6cd331
tamara <tamaradiana.ar@gmail.com>
2025-05-06 21:54:38 -0300

    .

--- ejercicios Parcial/relacionesValidas.hs
+++ ejercicios Parcial/relacionesValidas.hs
@@ -6,11 +6,16 @@ componentesDistintas (x:[]) | fst x == snd x = False
 componentesDistintas (x:xs) | fst x /= snd x = componentesDistintas (xs)
                             | otherwise = False
 
+--necesito alguna función que me recorra la lista de tuplas
+tuplasIguales :: (String,String) -> (String,String) -> Bool
+tuplasIguales (a,b) (c,d) | a == c && b == d = True
+                          | otherwise = False
 
-eliminarTuplasRepetidas :: [(String,String)] -> [(String,String)]
-eliminarTuplasRepetidas (x:[]) = (x:[])
-eliminarTuplasRepetidas (x:y:xs) | x == y = eliminarTuplasRepetidas (y:xs) 
-                               | otherwise = x:eliminarTuplasRepetidas (y:xs)
+sinTuplasRepetidas ::  [(String,String)] -> Bool
+sinTuplasRepetidas [] = True
+sinTuplasRepetidas (x:[]) = True
+sinTuplasRepetidas (x:xs) | tuplasIguales x head xs == False = sinTuplasRepetidas (xs)
+                          | otherwise = False
 
 {-relacionesValidas :: [(String,String)] -> Bool
 relacionesValidas [| componentesDistintas == True  && tuplasNoRepetidas == True = True
