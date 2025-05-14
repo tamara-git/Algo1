8f0892bc66384a6b33a75418f3dcea6333e4097e
Tamara <tamaradiana.ar@gmail.com>
2025-05-14 12:40:21 -0300

    modified:   "Gu\303\255a6/Testing.py" 	deleted:    "Gu\303\255a6/__pycache__/suma.cpython-312.pyc" 	new file:   "Gu\303\255a6/es_multiplo_de.py"

--- Guía6/Testing.py
+++ Guía6/Testing.py
@@ -15,3 +15,18 @@ if __name__ == '__main__':
     unittest.main(verbosity=2)
 
 
+from es_multiplo_de import es_multiplo_de
+
+class test_es_multiplo_de(unittest.TestCase):
+    def test_es_multiplo_cero_y_uno(self):
+        self.assertTrue(es_multiplo_de(0,5))
+        self.assertFalse(es_multiplo_de(1,5))
+        self.assertTrue(es_multiplo_de (15,1))
+
+    def test_es_multiplo_valores_iguales(self):
+        self.assertTrue(es_multiplo_de(8,8))
+        self.assertEqual(es_multiplo_de(12,12),True)
+        
+    def test_es_multiplo_valores_distintos(self):
+        self.assertTrue(es_multiplo_de(8,2))
+        self.assertFalse(es_multiplo_de(3,2))
---
+++ Guía6/es_multiplo_de.py
@@ -0,0 +1,4 @@
+def es_multiplo_de (n: int,m: int) -> bool:
+    return n % m == 0
+
+print (es_multiplo_de(2,3))
