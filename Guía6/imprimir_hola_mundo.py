53da99f1a932c3e2ca04f1dc312b82882801205d
Tamara <tamaradiana.ar@gmail.com>
2025-05-14 08:47:11 -0300

    new file:   "Gu\303\255a6/imprimir_hola_mundo.py"

---
+++ Guía6/imprimir_hola_mundo.py
@@ -0,0 +1,5 @@
+#Tengo que aclarar que no devuelve nada en el tipo.
+#Si uso el print adentro de la función no hace falta poner print (imprimir_hola_mundo ()) al final del código.
+
+def imprimir_hola_mundo () -> None:
+    print ("Hola mundo")
