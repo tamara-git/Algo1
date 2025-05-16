6e3269b3b379ff62bde31910e6c51c63dd1d5dbe
tamara <tamaradiana.ar@gmail.com>
2025-05-16 09:35:56 -0300

    factorial_de_dos

--- Guía6/factorial_de_dos.py
+++ Guía6/factorial_de_dos.py
@@ -1,4 +1,9 @@
-
+##factorial_de_dos()
+##problema factorial_2 () : Z {
+##requiere: {True}
+##asegura: {res = 2!}
 
 def factorial_de_dos() -> None:
-    print ()
+    print (2*1)
+
+factorial_de_dos()
