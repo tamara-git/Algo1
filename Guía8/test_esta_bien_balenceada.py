ebe60ae4cf736f14c1010a1f118040626d3e0d63
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-01 17:28:27 -0300

    esta_bien_balenceada creo que la terminé

--- Guía8/test_esta_bien_balenceada.py
+++ Guía8/test_esta_bien_balenceada.py
@@ -17,16 +17,27 @@ class Test_esta_bien_balanceada(unittest.TestCase):
         self.assertTrue(esta_bien_balanceada(pila))
 
     def test_False(self):
-        pila: Pila[str] = Pila()
-        pila.put('5')
-        pila.put('*')
-        pila.put(')')
-        pila.put('3')
-        pila.put(')')
-        pila.put('*')
-        pila.put('(')      
+        pila1: Pila[str] = Pila()
+        pila1.put('5')
+        pila1.put('*')
+        pila1.put(')')
+        pila1.put('3')
+        pila1.put(')')
+        pila1.put('*')
+        pila1.put('(')      
 
-        self.assertFalse(esta_bien_balanceada(pila))  
+        pila2: Pila[str] = Pila()
+        pila2.put(')')
+        pila2.put('*')
+        pila2.put('(')
+        pila2.put('3')
+        pila2.put(')')
+        pila2.put('*')
+        pila2.put('(')    
+
+        self.assertFalse(esta_bien_balanceada(pila1))  
+        self.assertFalse(esta_bien_balanceada(pila2))
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
