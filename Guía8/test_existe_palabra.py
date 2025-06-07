04ae3bf97d220e0665eb9d1feaff7c388c22e46c
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 12:21:32 -0300

    solo le paso el archivo

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -4,11 +4,9 @@ from existe_palabra import cadena_a_lista_palabras, existe_palabra
 
 class Test_existe_palabra(unittest.TestCase):
     def test_dos_lineas(self):
-        archivo = open("linea.txt", "r", encoding="utf-8")
         self.assertTrue(existe_palabra("linea.txt", "estas"))
         self.assertFalse(existe_palabra("linea.txt", "bien"))
-        archivo.close()
-
+        
     def test_cadena_hola_como_estas(self):
         self.assertEqual(cadena_a_lista_palabras("como estas\n"), ["como","estas"])
 
