58813626d75b6020736af677ca4ae710076755a2
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 12:16:23 -0300

    .

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -2,18 +2,15 @@ import unittest, os
 
 from existe_palabra import cadena_a_lista_palabras, existe_palabra
 
-
-class Test_cadena_a_lista_palabras(unittest.TestCase):
-    def test_cadena_hola_como_estas(self):
-        self.assertEqual(cadena_a_lista_palabras("como estas\n"), ["como","estas"])
-
-
 class Test_existe_palabra(unittest.TestCase):
     def test_dos_lineas(self):
         archivo = open("linea.txt", "r", encoding="utf-8")
-        archivo.close()
         self.assertTrue(existe_palabra("linea.txt", "estas"))
         self.assertFalse(existe_palabra("linea.txt", "bien"))
+        archivo.close()
+
+    def test_cadena_hola_como_estas(self):
+        self.assertEqual(cadena_a_lista_palabras("como estas\n"), ["como","estas"])
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
