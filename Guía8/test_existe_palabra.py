e588a480b30d05ed2b16449567e898347d44b823
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-06 23:41:06 -0300

    testCase corrección

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -2,13 +2,13 @@ import unittest
 
 from existe_palabra import existe_palabra, TextIO
 
-class Test_existe_palabra(unittest.Testcase):
+class Test_existe_palabra(unittest.TestCase):
     def test_una_linea(self):
         archivo: TextIO = open("linea.txt", "r", encoding = "utf-8")
-        
         
         self.assertTrue(existe_palabra("linea.txt", "verano"))
         self.assertFalse(existe_palabra("linea.txt", "invierno"))
 
+        archivo.close()
 
 
