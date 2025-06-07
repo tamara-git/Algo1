6e0063b508e6505756934896104df8c383dcf404
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 17:58:37 -0300

    nuevos cambios

--- Guía8/test_intercalar.py
+++ Guía8/test_intercalar.py
@@ -13,7 +13,7 @@ def copiar_cola(cola: Cola) -> Cola:
 
         return cola_copia
 
-def son_colas_iguales(c1: Cola, c2: Cola):
+def mismos_elementos(c1: Cola, c2: Cola) -> bool:
         lista_c1: list = []
         lista_c2: list = []
         c1_copia: Cola = copiar_cola(c1)
@@ -27,9 +27,34 @@ def son_colas_iguales(c1: Cola, c2: Cola):
             elemento_c2 = c2_copia.get()
             lista_c2.append(elemento_c2)
 
-        if lista_c1 == lista_c2 and len(lista_c1) == len(lista_c2):
+        for elemento1 in lista_c1:
+            for elemento2 in lista_c2:
+                if elemento1 == elemento2:
+                    return True
+                return False
+            
+def misma_longitud(c1: Cola, c2: Cola) -> bool:
+        lista_c1: list = []
+        lista_c2: list = []
+        c1_copia: Cola = copiar_cola(c1)
+        c2_copia: Cola = copiar_cola(c2)
+        
+        while not c1_copia.empty():
+            elemento_c1 = c1_copia.get()
+            lista_c1.append(elemento_c1)
+             
+        while not c2_copia.empty():
+            elemento_c2 = c2_copia.get()
+            lista_c2.append(elemento_c2)
+
+        if len(lista_c1) == len(lista_c2):
             return True
-        return False
+        return False 
+
+def son_colas_iguales(c1: Cola, c2: Cola) -> bool:
+    if mismos_elementos(c1,c2) == True and misma_longitud(c1,c2) == True:
+        return True
+    return False
 
 class Test_intercalar(unittest.TestCase):
     def test_cola_corta(self):
