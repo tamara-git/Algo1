46f5395c5b0f2c9a22c60c3db7bdc826dcf68b75
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 13:21:00 -0300

    .

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -8,8 +8,8 @@ class Test_existe_palabra(unittest.TestCase):
         archivo.write("como estas\nyo soy tamara")
         archivo.close()
 
-        self.assertTrue(existe_palabra("linea.txt", "estas"))
-        self.assertFalse(existe_palabra("linea.txt", "bien"))
+        self.assertTrue(existe_palabra("lineas.txt", "estas"))
+        self.assertFalse(existe_palabra("lineas.txt", "bien"))
 
     def test_cadena_hola_como_estas(self):
         self.assertEqual(cadena_a_lista_palabras("como estas\n"), ["como","estas"])
