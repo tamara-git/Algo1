af3333db5325c6eda44f9737fa598760f818deb8
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 18:05:27 -0300

    .

--- Guía8/intercalar.py
+++ Guía8/intercalar.py
@@ -19,6 +19,48 @@ def copiar_cola(cola: Cola) -> Cola:
         return cola_copia
 
 
+def mismos_elementos(c1: Cola, c2: Cola) -> bool:
+        lista_c1: list = []
+        lista_c2: list = []
+        c1_copia: Cola = copiar_cola(c1)
+        c2_copia: Cola = copiar_cola(c2)
+        
+        while not c1_copia.empty():
+            elemento_c1 = c1_copia.get()
+            lista_c1.append(elemento_c1)
+             
+        while not c2_copia.empty():
+            elemento_c2 = c2_copia.get()
+            lista_c2.append(elemento_c2)
+
+        for elemento1 in lista_c1:
+            for elemento2 in lista_c2:
+                if elemento1 == elemento2:
+                    return True
+                return False
+            
+def misma_longitud(c1: Cola, c2: Cola) -> bool:
+        lista_c1: list = []
+        lista_c2: list = []
+        c1_copia: Cola = copiar_cola(c1)
+        c2_copia: Cola = copiar_cola(c2)
+        
+        while not c1_copia.empty():
+            elemento_c1 = c1_copia.get()
+            lista_c1.append(elemento_c1)
+             
+        while not c2_copia.empty():
+            elemento_c2 = c2_copia.get()
+            lista_c2.append(elemento_c2)
+
+        if len(lista_c1) == len(lista_c2):
+            return True
+        return False 
+
+def son_colas_iguales(c1: Cola, c2: Cola) -> bool:
+    if mismos_elementos(c1,c2) == True and misma_longitud(c1,c2) == True:
+        return True
+    return False
 
 def intercalar(c1: Cola, c2: Cola) -> Cola:
     c1_copia: Cola = copiar_cola(c1)
--- Guía8/test_intercalar.py
+++ Guía8/test_intercalar.py
@@ -1,60 +1,6 @@
 import unittest
 
-from intercalar import intercalar, Cola
-
-def copiar_cola(cola: Cola) -> Cola:
-        cola_copia: Cola = Cola()
-
-        # Desencolo los elementos, hago la copia y restauro la cola
-        while not cola.empty():
-            elemento = cola.get()
-            cola_copia.put(elemento)
-            cola.put(elemento)
-
-        return cola_copia
-
-def mismos_elementos(c1: Cola, c2: Cola) -> bool:
-        lista_c1: list = []
-        lista_c2: list = []
-        c1_copia: Cola = copiar_cola(c1)
-        c2_copia: Cola = copiar_cola(c2)
-        
-        while not c1_copia.empty():
-            elemento_c1 = c1_copia.get()
-            lista_c1.append(elemento_c1)
-             
-        while not c2_copia.empty():
-            elemento_c2 = c2_copia.get()
-            lista_c2.append(elemento_c2)
-
-        for elemento1 in lista_c1:
-            for elemento2 in lista_c2:
-                if elemento1 == elemento2:
-                    return True
-                return False
-            
-def misma_longitud(c1: Cola, c2: Cola) -> bool:
-        lista_c1: list = []
-        lista_c2: list = []
-        c1_copia: Cola = copiar_cola(c1)
-        c2_copia: Cola = copiar_cola(c2)
-        
-        while not c1_copia.empty():
-            elemento_c1 = c1_copia.get()
-            lista_c1.append(elemento_c1)
-             
-        while not c2_copia.empty():
-            elemento_c2 = c2_copia.get()
-            lista_c2.append(elemento_c2)
-
-        if len(lista_c1) == len(lista_c2):
-            return True
-        return False 
-
-def son_colas_iguales(c1: Cola, c2: Cola) -> bool:
-    if mismos_elementos(c1,c2) == True and misma_longitud(c1,c2) == True:
-        return True
-    return False
+from intercalar import intercalar, son_colas_iguales, Cola
 
 class Test_intercalar(unittest.TestCase):
     def test_cola_corta(self):
