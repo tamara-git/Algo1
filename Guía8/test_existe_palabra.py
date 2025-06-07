da1f3cc07992b55bdc9dc900541af2728fc8d27e
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 13:23:19 -0300

    agrego un nuevo test.

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -11,6 +11,15 @@ class Test_existe_palabra(unittest.TestCase):
         self.assertTrue(existe_palabra("lineas.txt", "estas"))
         self.assertFalse(existe_palabra("lineas.txt", "bien"))
 
+    def test_tres_lineas(self):
+        archivo = open("texto.txt", "w", encoding = "utf-8")
+        archivo.write("como estas\n todo bien\nsoy tamara")
+        archivo.close()
+
+        self.assertTrue(existe_palabra("texto.txt", "bien"))
+        self.assertFalse(existe_palabra("texto.txt", "mal"))
+
+
     def test_cadena_hola_como_estas(self):
         self.assertEqual(cadena_a_lista_palabras("como estas\n"), ["como","estas"])
 
