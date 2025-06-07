6269f4f79dea4c8dbdcd4b329fb28df41aea8ee3
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-07 13:58:36 -0300

    el archivo es de tipo "w"

--- Guía8/test_cantidad_de_apariciones.py
+++ Guía8/test_cantidad_de_apariciones.py
@@ -3,14 +3,14 @@ from cantidad_de_apariciones import cantidad_de_apariciones
 
 class Test_cantidad_de_apariciones(unittest.TestCase):
     def test_2_apariciones(self):
-        archivo = open("archivo.txt", "r", encoding= "utf-8")
+        archivo = open("archivo.txt", "w", encoding= "utf-8")
         archivo.write("hermosa mañana verdad\ndemasiada hermosa para ser verdad")
         archivo.close()
 
         self.assertEqual(cantidad_de_apariciones("archivo.txt","hermosa"), 2)
 
     def test_1_aparicion(self):
-        archivo_2 = open("otro_archivo.txt", "r", encoding= "utf-8")
+        archivo_2 = open("otro_archivo.txt", "w", encoding= "utf-8")
         archivo_2.write("hermosa mañana verdad\ndemasiado para ser cierto")
         archivo_2.close()
 
