d32ea16bf74c9bc4b7832d2d1bb83c3fa2742ad6
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-06 23:42:21 -0300

    archivo creado para test

--- Guía8/test_existe_palabra.py
+++ Guía8/test_existe_palabra.py
@@ -6,8 +6,8 @@ class Test_existe_palabra(unittest.TestCase):
     def test_una_linea(self):
         archivo: TextIO = open("linea.txt", "r", encoding = "utf-8")
         
-        self.assertTrue(existe_palabra("linea.txt", "verano"))
-        self.assertFalse(existe_palabra("linea.txt", "invierno"))
+        self.assertTrue(existe_palabra(archivo, "verano"))
+        self.assertFalse(existe_palabra(archivo, "invierno"))
 
         archivo.close()
 
