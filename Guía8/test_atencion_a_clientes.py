c4924dde28e4fe772325fe8ff19270e6808cc166
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-08 18:14:51 -0300

    .

--- Guía8/test_atencion_a_clientes.py
+++ Guía8/test_atencion_a_clientes.py
@@ -26,16 +26,16 @@ def son_colas_iguales(c1: Cola[tuple[str, int, bool, bool]], c2: Cola[tuple[str,
 class Test_atencion_a_clientes(unittest.TestCase):
     def con_una_prioridad(self):
         cola: Cola[tuple[str,int,bool,bool]] = Cola()
-        cola.put(tuple["Pablo",47344554,False,True])
-        cola.put(tuple["Brunilda", 94256891, False, True])
-        cola.put(tuple["Kiara", 10245697, True, False])
-        cola.put(tuple["Tamara", 46683766, False, False])
+        cola.put(("Pablo",47344554,False,True))
+        cola.put(("Brunilda", 94256891, False, True))
+        cola.put(("Kiara", 10245697, True, False))
+        cola.put(("Tamara", 46683766, False, False))
 
         cola_ordenada: Cola[tuple[str,int,bool,bool]] = Cola()
-        cola_ordenada.put(tuple["Kiara", 10245697, True, False])
-        cola_ordenada.put(tuple["Pablo",47344554,False,True])
-        cola_ordenada.put(tuple["Brunilda", 94256891, False, True])
-        cola_ordenada.put(tuple["Tamara", 46683766, False, False])
+        cola_ordenada.put(("Kiara", 10245697, True, False))
+        cola_ordenada.put(("Pablo", 47344554,False,True))
+        cola_ordenada.put(("Brunilda", 94256891, False, True))
+        cola_ordenada.put(("Tamara", 46683766, False, False))
 
         self.assertTrue(son_colas_iguales(atencion_a_clientes(cola), cola_ordenada))
 
