6ba34f58404a0bd16341fd63fbd370bb7cf28630
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-08 18:19:41 -0300

    .

--- Guía8/atencion_a_clientes.py
+++ Guía8/atencion_a_clientes.py
@@ -61,3 +61,25 @@ def atencion_a_clientes(c: Cola[tuple[str, int, bool, bool]]) ->  Cola[tuple[str
         cola_ordenada.put(datos)
 
     return cola_ordenada
+
+
+def son_colas_iguales(c1: Cola[tuple[str, int, bool, bool]], c2: Cola[tuple[str, int, bool, bool]]) -> bool:
+    c1_copia: Cola[tuple[str, int, bool, bool]] = copiar_cola(c1)
+    c2_copia: Cola[tuple[str, int, bool, bool]] = copiar_cola(c2)
+    lista_c1: list[tuple[str, int, bool, bool]] = []
+    lista_c2: list[tuple[str, int, bool, bool]] = []
+
+    while not c1_copia.empty():
+        datos_c1: tuple[str, int, bool, bool] = c1_copia.get()
+        lista_c1 = lista_c1.append(datos_c1)
+        
+    while not c2_copia.empty():
+        datos_c2: tuple[str, int, bool, bool] = c2_copia.get()
+        lista_c2 = lista_c2.append(datos_c2)
+
+    
+    for indice in range(len(lista_c1)):
+        if lista_c1[indice] != lista_c2[indice] and len(lista_c1) != len(lista_c2):
+            return False
+    return True
+
