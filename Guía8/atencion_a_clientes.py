f2b6c87e8642c6bc7e22aa2e8f2ce369dbb1e98d
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-08 02:26:57 -0300

    atencion_al_cliente empezado

---
+++ Guía8/atencion_a_clientes.py
@@ -0,0 +1,33 @@
+'''
+La gerencia de un banco nos pide modelar la atención de los clientes usando una cola donde se van registrando
+los pedidos de atención. Cada vez que ingresa una persona a la entidad, debe completar sus datos en una pantalla que está a la
+entrada: Nombre y Apellido, DNI, tipo de cuenta (true si es preferencial o false en el caso contrario) y si tiene prioridad (true
+o false) por ser adulto +65, embarazada o con movilidad reducida.
+La atención a los clientes se da por el siguiente orden: primero las personas que tienen prioridad, luego las que tienen cuenta
+bancaria preferencial y por último el resto. Dentro de cada subgrupo de clientes, se respeta el orden de llegada.
+1. Dar una especificación para el problema planteado.
+2. Implementar atencion a clientes(in c : Cola[tuple[str, int, bool, bool]]) → Cola[tuple[str, int, bool, bool]] que dada la cola 
+de ingreso de clientes al banco devuelve la cola en la que van a ser atendidos.
+'''
+
+from queue import Queue as Cola  
+
+def copiar_cola(cola: Cola[tuple[str, int, bool, bool]]) -> Cola[tuple[str, int, bool, bool]]:
+    cola_copia: Cola[tuple[str, int, bool, bool]] = Cola()
+    cola_auxiliar: Cola[tuple[str, int, bool, bool]] = Cola()
+
+    while not cola.empty():
+        elemento_tupla: tuple[str, int, bool, bool] = cola.get()
+        cola_auxiliar.put(elemento_tupla)
+
+    while not cola_auxiliar.empty():
+        elemento_tupla: tuple[str, int, bool, bool] = cola_auxiliar.get()
+        cola_copia.put(elemento_tupla)
+        cola.put(elemento_tupla)
+
+    return cola_copia
+
+def atencion_a_clientes(c: Cola[tuple[str, int, bool, bool]]) -> Cola[tuple[str, int, bool, bool]]:
+    c_copia: Cola[tuple[str, int, bool, bool]] = copiar_cola(c)
+    c_copia
+
--- Guía8/test_carton_bingo.py
+++
@@ -1,4 +0,0 @@
-import unittest
-
-from jugar_carton_de_bingo import jugar_carton_de_bingo
-
