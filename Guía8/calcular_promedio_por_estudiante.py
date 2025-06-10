50553872fa03321c9600c385bbe60d5be3b5d1fa
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-10 15:11:32 -0300

    calcular_promedio_por_estudiante

--- Guía8/calcular_promedio_por_estudiante.py
+++ Guía8/calcular_promedio_por_estudiante.py
@@ -8,20 +8,30 @@ de notas)}
 }'''
 
 #[("luis",10).("luis",6),("fede",10)]
-def calcular_promedio_por_estudiante(notas:(list[tuple[str,float]])) -> dict[str,float]:
-    suma_notas : dict[str,float] = {}
-    cantidad_notas : dict[str,int] = {}
+def calcular_promedio_de_estudiante(calificaciones: list[tuple[str, float]], estudiante: str) -> float:
+    suma_notas: float = 0.0
+    cantidad_notas: int = 0
+    
+    for calificación in calificaciones:
+        calificante: str = calificación[0]
+        nota: float = calificación[1]
+        
+        if calificante == estudiante:
+            suma_notas += nota
+            cantidad_notas += 1
+    
+    if cantidad_notas == 0: return 0.0
+    return suma_notas / cantidad_notas
 
-    for estudiante, nota in notas:
-        if estudiante not in suma_notas.keys():
-            suma_notas[estudiante] = nota
-            cantidad_notas[estudiante] = 1
-        else:
-             suma_notas[estudiante] += nota
-             cantidad_notas[estudiante] += 1
 
-    promedios: dict[str, float] = {}
-    for estudiante in suma_notas.keys():
-        promedios[estudiante] = suma_notas[estudiante] / cantidad_notas[estudiante]
-
-        return promedios
+def calcular_promedio_por_estudiante(calificaciones: list[tuple[str, float]]) -> dict[str, float]:
+    notas: dict[str, float] = {}
+    
+    for calificación in calificaciones:
+        estudiante: str = calificación[0]
+        
+        if estudiante not in notas.keys():
+            promedio: float = calcular_promedio_de_estudiante(calificaciones, estudiante)
+            notas[estudiante] = promedio
+    
+    return notas
---
+++ Guía8/visitar_sitio.py
@@ -0,0 +1,22 @@
+'''
+Se debe desarrollar un navegador web muy simple que debe llevar un registro de los sitios web visitados por los
+usuarios del sistema. El navegador debe permitir al usuario navegar hacia atrás en la historia de navegación.
+1. Crea un diccionario llamado historiales que almacenará el historial de navegación para cada usuario. Las claves del
+diccionario serán los nombres de usuario y los valores serán pilas de String.
+2. Implementar una solución para el siguiente problema.
+problema visitar sitio (inout historiales: Diccionario⟨seq⟨Char⟩, P ila[seq⟨Char⟩]⟩, in usuario: seq⟨Char⟩, in sitio: seq⟨Char⟩)
+{
+requiere: {Ninguno de los Strings de los parámetros es vacío}
+asegura: {Si usuario es una de las claves de historiales@pre, entonces se agrega sitio a su pila de historiales@pre[usuario]}
+asegura: {Si usuario no es una de las claves de historiales@pre, entonces historiales[usuario] es igual a la pila
+que tiene solo el elemento sitio}
+asegura: {No se modifica ningún otro historial salvo, si existe, el de usuario}
+asegura: {Todos los pares clave-valor de historiales@pre están en historiales}
+asegura: {Todos los pares clave-valor de historiales están en historiales@pre, salvo historiales[usuario] que
+podría no existir en historiales@pre}
+}
+'''
+
+from queue import LifoQueue as Pila
+
+def visitar_sitio(historiales: dict[str, Pila[str]]) -> 
