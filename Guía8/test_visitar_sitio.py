cfdf1bd8dc2f02621be73d1e5dae992467517cb8
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-11 14:15:53 -0300

    .

---
+++ Guía8/test_visitar_sitio.py
@@ -0,0 +1,21 @@
+import unittest
+
+from visitar_sitio import visitar_sitio
+from queue import LifoQueue as Pila
+
+class Test_visitar_sitio(unittest.Testcase):
+    def test_un_usuario(self):
+
+        historiales_inicio: dict[str,Pila[str]] = {}
+
+        historial_kiara: Pila[str] = Pila()
+        historial_kiara.put("google.com")
+    
+        historiales: dict[str,Pila[str]] = {
+            "Kiara": historial_kiara
+        }
+
+        self.assertEqual(visitar_sitio(historiales_inicio, "Kiara", "pinterest.com"), historiales)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
