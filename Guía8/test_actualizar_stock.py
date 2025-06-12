87842e5046449e481573350f7b46593369db2fe0
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-11 21:46:58 -0300

    .

--- Guía8/test_actualizar_stock.py
+++ Guía8/test_actualizar_stock.py
@@ -7,8 +7,19 @@ class Test_actualizar_stock(unittest.TestCase):
         inventario: dict[str, dict[str, float | int]] = {
             "remera": {"precio": 10000, "cantidad": 6} 
         }
+    import unittest
 
-        self.assertEqual(actualizar_stock(inventario, "remera", 4), inventario)
+from actualizar_stock import actualizar_stock
 
+class Test_actualizar_stock(unittest.TestCase):
+    def test_un_producto(self):
+        inventario: dict[str, dict[str, float | int]] = {
+            "remera": {"precio": 10000, "cantidad": 6} 
+        }
+
+        actualizar_stock(inventario, "remera", 4)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
 if __name__ == "__main__":
     unittest.main(verbosity=2)
