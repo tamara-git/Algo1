6be27998f84b134bba92dfc4f6cf5b5c5065605b
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-11 21:36:10 -0300

    test actualizar_stock

---
+++ Guía8/actualizar_stock.py
@@ -0,0 +1,19 @@
+'''problema actualizar stock (inout inventario: Diccionario ⟨ seq⟨Char⟩, Diccionario⟨ seq⟨Char⟩, T ⟩⟩, in nombre: seq⟨Char⟩,
+in cantidad: R) {
+requiere: {T ∈ [Z, R]}
+requiere: {cantidad ≥ 0}
+requiere: {nombre es una clave existente en el inventario}
+requiere: {Ninguno de los Strings de los par´ametros es vacío}
+asegura: {Todos los pares clave-valor de inventario@pre están tal cual en inventario, con excepción del que tiene
+como clave nombre}
+asegura: {Todos los pares clave-valor de inventario están en inventario@pre}
+asegura: {En inventario, el valor asociado a la clave nombre, tendrá el mismo precio que antes y la cantidad será
+cantidad}
+}'''
+
+def actualizar_stock(inventario: dict[str, dict[str, float | int]], nombre: str, cantidad: float):
+    
+    if nombre in inventario.keys():
+        inventario[nombre] = {
+        "cantidad": cantidad
+        }
---
+++ test_actualizar_stock.py
@@ -0,0 +1,11 @@
+import unittest
+
+from actualizar_stock import actualizar_stock
+
+class Test_actualizar_stock(unittest.TestCase):
+    def test_un_producto(self):
+        inventario: dict[str, dict[str, float | int]] = {
+            "remera": {"precio": 10000, "cantidad": 6} 
+        }
+
+        actualizar_stock(inventario, "remera", 4)
