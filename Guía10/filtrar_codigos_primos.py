d3efd3f45648eeb8e6ee6ca160c82f310eee772d
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-14 20:51:59 -0300

    filtrar_codigos_primos hecho

--- Guía10/filtrar_codigos_primos.py
+++ Guía10/filtrar_codigos_primos.py
@@ -9,12 +9,30 @@ tres d´ıgitos son: 2, 3, 5, 101, 103, 107, etc.
 problema filtrar codigos primos (in codigos barra : seq⟨Z⟩ ) : seq⟨Z⟩ {
 requiere: {Todos los enteros de codigos barra tienen, por lo menos, 3 d´ıgitos.}
 requiere: {No hay elementos repetidos en codigos barra.}
-asegura: {Los ´ultimos 3 d´ıgitos de cada uno de los elementos de res forman un n´umero primo.}
+asegura: {Los últimos 3 dígitos de cada uno de los elementos de res forman un número primo.}
 asegura: {Todos los elementos de codigos barra cuyos ´ultimos 3 d´ıgitos forman un n´umero primo est´an en res.}
 asegura: {Todos los elementos de res est´an en codigos barra.}
 }
 '''
-def es_primo(numero: int) -> bool:
+
+def devolver_ultimos_3digitos(numero: int) -> int:
+    ultimos_3digitos: str = ""
+    ultimos_digitos: str = ""
+
+    digitos: str = str(numero)
+    if len(digitos) > 3:
+        for caracter in range(len(digitos)-3, len(digitos)):
+            ultimos_3digitos += digitos[caracter]
+        return int(ultimos_3digitos)
+    else:
+        for caracter in range(len(digitos)):
+            ultimos_3digitos += digitos[caracter]
+        return int(ultimos_3digitos)
+
+    
+
+
+def es_primo_ultimos_3digitos(numero: int) -> bool:
     variable: int = 1
     divisor: int = 0
     while variable <= numero:
@@ -30,7 +48,16 @@ def es_primo(numero: int) -> bool:
 
 
 def filtrar_codigos_primos(codigos_barra:(list[int])) -> list[int]:
-    lista_primos
-
+    lista_codigos_primos_aux: list[int] = []
+    lista_codigos_primos: list[int] = []
     for numero in codigos_barra:
-        if es_primo(numero):
+        if es_primo_ultimos_3digitos(devolver_ultimos_3digitos(numero)):
+            lista_codigos_primos_aux.append(devolver_ultimos_3digitos(numero))
+    
+    for numero_primo in lista_codigos_primos_aux:
+        if numero_primo not in lista_codigos_primos:
+            lista_codigos_primos.append(numero_primo)
+
+    return lista_codigos_primos
+
+print(filtrar_codigos_primos([10, 11, 24101, 13002, 5, 240242,4]))
