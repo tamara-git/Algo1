96f0cd278fcf0fb8133a1dc29fbd021acf5886ff
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-15 20:47:31 -0300

    .

--- Guía10/test_responsable_por_turno.py
+++ Guía10/test_responsable_por_turno.py
@@ -4,7 +4,7 @@ from un_responsable_por_turno import un_responsable_por_turno
 
 class Test_responsable_por_turno(unittest.TestCase):
     def test_un_responsable(self):
-        grilla_horaria: list[list[int]] = [["a","a","a","a","a","a","a"],
+        grilla_horaria: list[list[str]] = [["a","a","a","a","a","a","a"],
                                            ["a","a","a","a","a","a","a"],
                                            ["a","a","a","a","a","a","a"],
                                            ["a","a","a","a","a","a","a"],
