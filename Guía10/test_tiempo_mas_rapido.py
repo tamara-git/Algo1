1d62d80187d6b3b01a9a6dfec5d789346bc22435
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-16 14:46:18 -0300

    mas test

--- Guía10/test_tiempo_mas_rapido.py
+++ Guía10/test_tiempo_mas_rapido.py
@@ -9,6 +9,11 @@ class Test_tiempo_mas_rapido(unittest.TestCase):
 
         self.assertEqual(tiempo_mas_rapido(tiempos_salas), res)
 
+    def test_dos_tiempos_rapidos(self):
+        tiempos_salas: list[int] = [4,6,2,7,9,3,24,6,8,13,24]
+        res: int = 6
+
+        self.assertEqual(tiempo_mas_rapido(tiempos_salas), res)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
