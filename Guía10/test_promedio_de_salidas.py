a457b99d7c2d78312452a71cf558b5794586b2d8
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-16 11:45:55 -0300

    nuevos cambios

--- Guía10/test_promedio_de_salidas.py
+++ Guía10/test_promedio_de_salidas.py
@@ -5,11 +5,15 @@ from promedio_de_salidas import calcular_promedio, promedio_de_salidas
 class Test_promedio_de_salidas(unittest.TestCase):
     def test_un_participante(self):
         registro: dict[str,list[int]] = {
-            "carla": [30,40,60,61,60.3]
+            "carla": [30,40,60,61,60.3],
+            "marta": [5,40.5,20.3],
+            "julia": [10.5,38.20]
         }
 
         res: dict[str,tuple[int,float]] = {
-            "carla": (3,(30 + 40 + 60)/3)
+            "carla": (3,(30 + 40 + 60)/3),
+            "marta": (3,(5 + 40.5 + 20.3)/3),
+            "julia": (2,(10.5 + 38.20)/2)
         }
 
         self.assertAlmostEqual(promedio_de_salidas(registro), res)
@@ -19,7 +23,7 @@ class Test_promedio_de_salidas(unittest.TestCase):
             "carla": [30,40,60,61,60.3]
         }
 
-        promedio: float = (30+40+60)/3
+        promedio: list[float] = [(30+40+60)/3]
         self.assertAlmostEqual(calcular_promedio(registro), promedio)
 
 
