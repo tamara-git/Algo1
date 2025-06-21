85150ca4008f8cc6e1f52341cb85876c194b0469
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-21 20:23:42 -0300

    .

--- integradorHaskell/masRepetido.hs
+++ integradorHaskell/masRepetido.hs
@@ -33,7 +33,7 @@ cantidadAparicionesTablero e (x:xs) | cantidadDeApariciones e x > 0 = cantidadDe
 tuplasNumeroConAparicion :: Fila -> [(Int, Int)] 
 tuplasNumeroConAparicion [x] = [(x, 1)]
 tuplasNumeroConAparicion (x:xs) | cantidadDeApariciones x xs == 0 = [(x,1)] ++ tuplasNumeroConAparicion xs
-                                | otherwise = eliminarRepetidos(x , [(x, 1 + cantidadDeApariciones x xs)] ++ tuplasNumeroConAparicion xs)
+                                | otherwise = eliminarRepetidos(x , ([(x, 1 + cantidadDeApariciones x xs)] ++ tuplasNumeroConAparicion xs))
 
 
 eliminarRepetidos ::  Int -> [(Int,Int)] -> [(Int,Int)]
