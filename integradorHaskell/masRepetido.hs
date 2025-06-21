3b87ac1f7bfdfc07ae1604c161ff6898d4dd37dd
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-21 09:41:30 -0300

    masRepetido

---
+++ integradorHaskell/masRepetido.hs
@@ -0,0 +1,27 @@
+{-. Implementar la funci´on masRepetido :: Tablero ->Int
+problema masRepetido (t: Tablero) : Z {
+requiere: {El tablero t es un tablero bien formado, es decir, la longitud de todas las filas es la misma, y tienen al
+menos un elemento}
+requiere: {Existe al menos una columna en el tablero t }
+requiere: {El tablero t no es vac´ıo, todos los n´umeros del tablero son positivos, mayor estricto a 0}
+asegura: {res es igual al n´umero que m´as veces aparece en un tablero t. Si hay empate devuelve cualquiera de ellos}
+}-}
+
+type Fila = [Int]
+type Tablero = [Fila]
+
+
+cantidadDeApariciones :: Int -> Fila -> Int
+cantidadDeApariciones e (x:xs) | x == e = 1 + cantidadDeApariciones xs
+                               | otherwise = cantidadDeApariciones xs
+
+cantidadDeAparicionesTablero :: Int -> Tablero -> Int
+cantidadDeAparicionesTablero e (x:xs) | cantidadDeApariciones x e > 0 == cantidadDeApariciones x e + cantidadDeAparicionesTablero xs e
+                                      | otherwise = cantidadDeAparicionesTablero xs e
+
+
+masRepetido :: Tablero -> Int
+masRepetido [x] | (cantidadDeApariciones e x > cantidadDeApariciones y x) = e
+                | otherwise = y
+masRepetido (x:xs) | cantidadDeAparicionesTablero e (x:xs) > cantidadDeAparicionesTablero y (x:xs) = e
+                   | otherwise = y
