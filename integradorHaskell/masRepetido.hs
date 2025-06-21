34ceef0a31df257c305eb6a14ab89022ab4471a5
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-21 09:43:01 -0300

    .

--- integradorHaskell/masRepetido.hs
+++ integradorHaskell/masRepetido.hs
@@ -12,8 +12,8 @@ type Tablero = [Fila]
 
 
 cantidadDeApariciones :: Int -> Fila -> Int
-cantidadDeApariciones e (x:xs) | x == e = 1 + cantidadDeApariciones xs
-                               | otherwise = cantidadDeApariciones xs
+cantidadDeApariciones e (x:xs) | x == e = 1 + cantidadDeApariciones e xs
+                               | otherwise = cantidadDeApariciones e xs
 
 cantidadDeAparicionesTablero :: Int -> Tablero -> Int
 cantidadDeAparicionesTablero e (x:xs) | cantidadDeApariciones e x > 0 == cantidadDeApariciones e x + cantidadDeAparicionesTablero e xs
