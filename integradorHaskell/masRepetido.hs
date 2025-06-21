9140fb679f5d8431a4bfbdf57afeeda0ebab86c0
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-21 19:21:18 -0300

    .

--- integradorHaskell/masRepetido.hs
+++ integradorHaskell/masRepetido.hs
@@ -31,5 +31,5 @@ cantidadAparicionesTablero e (x:xs) | cantidadApariciones e x > 0 = cantidadApar
 tuplasNumeroConAparicion :: Fila -> [(Int, Int)] 
 tuplasNumeroConAparicion [x] = (x, 1)
 tuplasNumeroConAparicion (x:xs) | cantidadApariciones x xs == 0 = [(x,1)]
-                                | otherwise = [(x, 1 + cantidadApariciones xs)]
+                                | otherwise = [(x, 1 + cantidadApariciones x xs)] 
 
