892d481138bf21e57638d1d05b0e5f5cb534557f
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-22 10:53:56 -0300

    .

--- integradorHaskell/masRepetido.hs
+++ integradorHaskell/masRepetido.hs
@@ -47,5 +47,5 @@ eliminarTupla a (x:xs) | fst x == a = eliminarTupla a xs
                        | otherwise = x: eliminarTupla a xs 
 
 eliminarTuplasConFstRepetidas :: [(Int,Int)] -> [(Int,Int)]
-eliminarTuplasConFstRepetidas (x:xs) | perteneceFst fst x xs == True = eliminarTupla (fst x) xs
+eliminarTuplasConFstRepetidas (x:xs) | perteneceFst (fst x) xs == True = eliminarTupla (fst x) xs
                                      | otherwise = [x] ++ eliminarTuplasConFstRepetidas xs
