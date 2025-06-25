a9778a4f628771e899fd982dfade626bdbfde823
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-25 20:21:10 -0300

    ,

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -94,6 +94,6 @@ sumarColumnas [x] [y] n = [(n, 0 + y)]
 sumarColumnas (x:xs) (y:ys) n = [(n, 0 + y)] ++ sumarColumnas xs ys n
 
 posicion :: Tablero -> [Int] -> Int -> [(Int,Int)]
-posicion [] [] = []
+posicion [] [] n = []
 posicion [x] [y] n = sumarColumnas x [y] (filas [x])
 posicion (x:xs) (y:ys) n = sumarColumnas x [y] (filas [x]) ++ posicion xs ys (filas xs)
