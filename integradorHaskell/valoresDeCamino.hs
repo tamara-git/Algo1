07a7c855e886b4aac5ea427414a856119ab8791e
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-24 21:18:23 -0300

    .

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -79,7 +79,7 @@ armarListaColumna (x:xs) = [head x] ++ armarListaColumna xs
 quitarColumna :: Tablero -> [Int]
 quitarColumna [x] = tail(aplanar [x])
 
-devuelveColumna :: Tablero -> Int -> Int
+{-devuelveColumna :: Tablero -> Int -> Int
 devuelveColumna [x] e = perteneceYDevuelvePosicion e (armarListaColumna [x])
 devuelveColumna (x:xs) e | pertenece e (armarListaColumna (x:xs)) = perteneceYDevuelvePosicion e (armarListaColumna (x:xs))
                          | otherwise =
@@ -88,7 +88,7 @@ devuelveColumna (x:xs) e | pertenece e (armarListaColumna (x:xs)) = perteneceYDe
 posicion ::  Tablero -> Int -> (Int,Int)
 posicion [x] e = (devuelveFila [x] e, devuelveColumna [x] e)
 posicion (x:xs) e = (devuelveFila (x:xs) e, devuelveColumna (x:xs) e)
-
+-}
 
 {-valoresDeCamino :: Tablero -> Camino -> [Int]
 valoresDeCamino [x] [(a,b)] = devuelveFila (x:xs)
