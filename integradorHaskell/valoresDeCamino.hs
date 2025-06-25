d66bc0e57c290d82062ba7a1f8fa55f6fbdbce0f
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-25 18:39:24 -0300

    .

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -106,8 +106,8 @@ sucesionHasta n =  sucesionHasta (n-1) ++ [n]
 
 
 sumarColumnas :: [Int] -> [Int] -> [(Int,Int)]
-sumarColumnas [x] sucesionHasta n = [(1,0 + head (sucesionHasta n))]
-sumarColumnas (x:xs) sucesionHasta n = [(1,0 + head (sucesionHasta n))] ++ sumarColumnas xs (tail(sucesionHasta n))
+sumarColumnas [x] (sucesionHasta n) = [(1,0 + head (sucesionHasta n))]
+sumarColumnas (x:xs) (sucesionHasta n) = [(1,0 + head (sucesionHasta n))] ++ sumarColumnas xs (tail(sucesionHasta n))
 
 
 {-posicionIdea :: Tablero -> [(Int,Int)]
