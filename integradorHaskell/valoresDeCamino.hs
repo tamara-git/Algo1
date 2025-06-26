91fe6a623f4531b62f1580cd2ce63461e92156b8
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-25 23:10:51 -0300

    .

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -53,5 +53,5 @@ matrizPosiciones [x] = posicionTablero [x] 1
 matrizPosiciones (x:xs) = posicionTablero (x:xs) 1
 
 
-valoresDeCamino :: Tablero -> Camino -> [Int]
+--valoresDeCamino :: Tablero -> Camino -> [Int]
 --valoresDeCamino [x] [(a,b)] | matrizPosiciones [x] == [(a,b)] = 
