314aa15875d9ca50db2895348dae4439d8e75516
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-25 22:27:10 -0300

    -

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -58,4 +58,4 @@ posicionFila (x:xs) n m = [(m, 0+n)] ++ posicionFila xs (n+1) m
 
 posicionTablero :: Tablero -> Int -> [(Int,Int)]
 posicionTablero [x] n = posicionFila x 1 1 
-posicionTablero (x:xs) n = posicionFila x n 1 ++ posicionFila xs (n+1) 1    
+posicionTablero (x:xs) n = posicionFila x n 1 ++ posicionTablero xs (n+1) 1    
