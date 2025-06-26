201f6843bdce4205631e055e58e73e803641d264
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-26 11:31:35 -0300

    -

--- integradorHaskell/valoresDeCamino.hs
+++ integradorHaskell/valoresDeCamino.hs
@@ -53,7 +53,7 @@ eliminarPrimerElemento (x:xs) = xs
 
 valoresDeCaminoAux :: Tablero -> Camino -> [Int]
 valoresDeCaminoAux [x] [y] | head (posicionFila x 1 1) == y = [head x]
-                           | otherwise = valoresDeCaminoAux [tail x] [y]
+                           | otherwise = valoresDeCaminoAux [eliminarPrimerElemento x] [y]
  
 
 
