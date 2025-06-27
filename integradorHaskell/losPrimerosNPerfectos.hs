e114c43f9dba56086ac2991d1d9e147876765005
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-27 18:27:57 -0300

    .

--- integradorHaskell/losPrimerosNPerfectos.hs
+++ integradorHaskell/losPrimerosNPerfectos.hs
@@ -22,7 +22,8 @@ accederAElem (x:xs) i = accederAElem xs (i-1)
 
 sumarElementos :: [Int] -> Int -> Int
 sumarElementos [x] i = x
-sumarElementos (x:xs) i n = | i <= n = accederAElem (x:xs) i + sumarElementos (x:xs) (i+1)
+sumarElementos (x:xs) i n | i <= n = accederAElem (x:xs) i + sumarElementos (x:xs) (i+1)
+                          | otherwise = 0
 
 {-losPrimerosNPerfectos :: Int -> [Int]
 losPrimerosNPerfectos n | -}
