f2f5fca37c27b2dcb03f8f7363c1bc5ac174f942
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-27 16:41:19 -0300

    .

---
+++ integradorHaskell/divisoresPropios.hs
@@ -0,0 +1,15 @@
+{-Implementar la funci´on divisoresPropios :: Int ->[Int]
+problema divisoresPropios (n: Z) : seq⟨Z⟩ {
+requiere: {n > 0}
+asegura: {res contiene a todos los divisores propios de n, ordenados de menor a mayor}
+asegura: {res no tiene elementos repetidos}
+asegura: {res no contiene a ning´un elemento que no sea un divisor propio de n}
+}
+-}
+
+divisordeN :: Int -> Int -> [Int]
+divisordeN i n | mod i n == 0 = [i] ++ divisordeN (i+1) n
+               | otherwise = divisordeN (i+1) n 
+
+divisoresPropios :: Int -> [Int]
+divisoresPropios
