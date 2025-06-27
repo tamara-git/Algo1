de62454fb3666cc3294a9d5e423f279923270699
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-27 18:15:04 -0300

    .

---
+++ integradorHaskell/losPrimerosNPerfectos.hs
@@ -0,0 +1,21 @@
+{-Ejercicio 11. Implementar la funci´on losPrimerosNPerfectos :: Int ->[Int]
+problema losPrimerosNPerfectos (n: Z) : seq⟨Z⟩ {
+requiere: {n > 0}
+asegura: {|res| = n}
+asegura: {res es la lista de los primeros n n´umeros perfectos, de menor a mayor}
+}
+Por cuestiones de tiempos de ejecuci´on, no les recomendamos que prueben este ejercicio con un n > 4.-}
+
+
+divisoresPropios :: Int -> [Int]
+divisoresPropios n = divisoresDeN n 1 
+where divisoresDeN n i | mod n i == 0 = i:divisoresDeN n (i+1)
+                       | otherwise = divisoresDeN n (i+1)
+
+accederAElem :: [Int] -> Int
+accederAElem [x] 1 = x
+accederAElem (x:xs) 1 = x
+accederAElem (x:xs) i = accederAElem xs (i-1)
+
+losPrimerosNPerfectos :: Int -> [Int]
+losPrimerosNPerfectos n | 
