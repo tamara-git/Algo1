5af80ddd3bb303d057b5bb7fef988a93df46a0ae
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-27 16:45:06 -0300

    .

--- integradorHaskell/divisoresPropios.hs
+++ integradorHaskell/divisoresPropios.hs
@@ -8,7 +8,7 @@ asegura: {res no contiene a ning´un elemento que no sea un divisor propio de n}
 -}
 
 divisordeN :: Int -> Int -> [Int]
-divisordeN i n | (i <= n) and (mod n i == 0) = [i] ++ divisordeN (i+1) n
+divisordeN i n | (i <= n) && (mod n i == 0) = [i] ++ divisordeN (i+1) n
                | otherwise = divisordeN (i+1) n 
 
 {-divisoresPropios :: Int -> [Int]
