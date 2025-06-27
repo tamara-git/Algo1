c98eceb1b12a9cae33f62bb955f545c6266048bf
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-27 13:24:51 -0300

    .

--- integradorHaskell/esCaminoFibo.hs
+++ integradorHaskell/esCaminoFibo.hs
@@ -16,6 +16,7 @@ True.
 fibonacci :: Int -> [Int]
 fibonacci 0 = [0]
 fibonacci 1 = [0,1]
+fibonacci 2 = [0,1,1]
 fibonacci n = fibonacci (n-1) ++ fibonacci (n-2)
 
 accederElem :: [Int] -> Int -> Int
