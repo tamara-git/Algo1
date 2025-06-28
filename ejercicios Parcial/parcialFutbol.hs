904df42301fc198fa067fede92f5017b249896bb
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-28 15:14:29 -0300

    .

--- ejercicios Parcial/parcialFutbol.hs
+++ ejercicios Parcial/parcialFutbol.hs
@@ -104,7 +104,7 @@ accederAElemPorIndice (x:xs) 1 = x
 accederAElemPorIndice (x:xs) i = accederAElemPorIndice xs (i-1)
 
 
-indiceGoleador :: ([Char],[Char]) -> [([Char],[Char])] -> Int -> Int
+indiceGoleador :: [Char] -> [([Char],[Char])] -> Int -> Int
 indiceGoleador goleador [x] 1 | goleador == fst (accederAElemPorIndice [x] 1) = 1
                               | otherwise = 0
 indiceGoleador goleador (x:xs) i | goleador == fst (accederAElemPorIndice (x:xs) i) = i
