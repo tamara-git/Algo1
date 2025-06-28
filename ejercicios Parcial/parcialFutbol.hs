add07ff87b6c122c3e5d7eaedced4d713de7c322
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-28 16:18:14 -0300

    -

--- ejercicios Parcial/parcialFutbol.hs
+++ ejercicios Parcial/parcialFutbol.hs
@@ -136,7 +136,7 @@ mayorElemento (x:xs) | x >= mayorElemento xs = x
 indiceMayorElemento :: [Int] -> Int -> Int
 indiceMayorElemento [x] 1 = 1
 indiceMayorElemento (x:xs) i | mayorElemento (x:xs) == x = i
-                             | otherwise = indiceMayorElemento (i+1)
+                             | otherwise = indiceMayorElemento (x:xs) (i+1)
 
 
 {-botinDeOro :: [([Char],[Char])] -> [Int] -> [Char]
