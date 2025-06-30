5c760951da9c01e859e8367355e1473b2d6e48e9
@tamaradiana.ar <tamaradiana.ar@gmail.com>
2025-06-29 22:55:48 -0300

    .

--- ejercicios Parcial/otroParcial.hs
+++ ejercicios Parcial/otroParcial.hs
@@ -89,7 +89,7 @@ indiceAbecedario (x:xs) elemento | elemento == x = 1
 
 esMenor :: [Char] -> Bool
 esMenor [] = False 
-esMenor (x:xs) | indiceAbecedario abecedario x < indiceAbecedario abecedario (head xs) = esMenor xs
+esMenor (x:xs) | (indiceAbecedario abecedario x) < (indiceAbecedario abecedario (head xs)) = esMenor xs
                | otherwise = False 
 
 {-palabraOrdenada :: [Char] -> Bool
