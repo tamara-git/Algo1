bc8b68e3808ca60845e6b54539f743b232681591
LeChuck <3headed@monkey.com>
2025-10-20 19:32:20 -0300

    jm

--- Guía8/archivos_Test.py
+++ Guía8/archivos_Test.py
@@ -9,5 +9,10 @@ class Test_contar_lineas(unittest.TestCase):
         self.assertEqual(resultado, 1)
 
 
+# #class Test_contar_lineas(unittest.TestCase):
+#     def test_contar_lineas_archivo_vacio(self):
+#         ruta_archivo: str = os.path.join("archivos_test","archivo_con_una_linea_vacia.txt")
+#         resultado = contar_lineas(ruta_archivo)
+#         self.assertEqual(resultado, 1)
 if __name__ == '__main__':
     unittest.main(verbosity=2)
